25a5f9a5544e353c0fe22e241c0ee4f921051af3
Niccolò Bonacchi <nbonacchi@gmail.com>
2018-09-12 15:46:53 +0100

    restored global_timer_example

--- pybpod_projects/IBL/tasks/global_timer_example/global_timer_example.py
+++ pybpod_projects/IBL/tasks/global_timer_example/global_timer_example.py
@@ -5,7 +5,6 @@
 Example adapted from Josh Sanders' original version on Sanworks Bpod repository
 """
 from pybpodapi.protocol import Bpod, StateMachine
-import time
 
 
 my_bpod = Bpod()
@@ -16,58 +15,29 @@ sma = StateMachine(my_bpod)
 sma.set_global_timer_legacy(timer_id=1, timer_duration=3)
 
 sma.add_state(
-	state_name='TimerTrig',  # Trigger global timer
-	state_timer=0,
-	state_change_conditions={Bpod.Events.Tup: 'Port1Lit'},
-	output_actions=[(Bpod.OutputChannels.GlobalTimerTrig, 1)])
+    state_name='TimerTrig',  # Trigger global timer
+    state_timer=0,
+    state_change_conditions={Bpod.Events.Tup: 'Port1Lit'},
+    output_actions=[(Bpod.OutputChannels.GlobalTimerTrig, 1)])
 
 sma.add_state(
-	state_name='Port1Lit',  # Infinite loop (with next state). Only a global timer can save us.
-	state_timer=.25,
-	state_change_conditions={Bpod.Events.Tup: 'Port3Lit', Bpod.Events.GlobalTimer1_End: 'exit'},
-	output_actions=[('Valve', 2)])
+    state_name='Port1Lit',  # Infinite loop (with next state). Only a global timer can save us.
+    state_timer=.25,
+    state_change_conditions={Bpod.Events.Tup: 'Port3Lit',
+                             Bpod.Events.GlobalTimer1_End: 'exit'},
+    output_actions=[('Valve', 2)])
 
 sma.add_state(
-	state_name='Port3Lit',
-	state_timer=.25,
-	state_change_conditions={Bpod.Events.Tup: 'Port1Lit', Bpod.Events.GlobalTimer1_End: 'exit'},
-	output_actions=[(Bpod.OutputChannels.PWM3, 255)])
+    state_name='Port3Lit',
+    state_timer=.25,
+    state_change_conditions={Bpod.Events.Tup: 'Port1Lit',
+                             Bpod.Events.GlobalTimer1_End: 'exit'},
+    output_actions=[(Bpod.OutputChannels.PWM3, 255)])
 
 my_bpod.send_state_machine(sma)
 
 my_bpod.run_state_machine(sma)
 
 print("Current trial info: {0}".format(my_bpod.session.current_trial) )
-
-
-ambient_module = [x for x in my_bpod.modules if x.name == 'AmbientModule1'][0]
-ambient_module.start_module_relay()
-my_bpod.bpod_modules.module_write(ambient_module, 'R')
-reply = my_bpod.bpod_modules.module_read(ambient_module, 12)
-# time.sleep(2)
-ambient_module.stop_module_relay()
-print('reply:', reply)
-import numpy as np
-Measures = {'Temperature_C': np.frombuffer(bytes(reply[:4]), np.float32),
-			'AirPressure_mb': np.frombuffer(bytes(reply[4:8]), np.float32) / 100,
-			'RelativeHumidity': np.frombuffer(bytes(reply[8:]), np.float32)
-}
-print(Measures)
-# my_bpod.bpod_module.start_module_relay('AmbientModule1')
-# my_bpod.bpod_module.module_write('AmbientModule1', 'R')
-# reply = my_bpod.bpod_module.module_read('AmbientModule1', 12)
-# my_bpod.bpod_module.stop_modules_relay()
-
-# print(reply)
-
-
-
-# ModuleWrite(ModuleName, 'R', 'uint8');
-# Measures = struct;
-# Reply = ModuleRead(ModuleName, 12, 'uint8');
-# Measures.Temperature_C  = typecast(Reply(1:4), 'single');
-# Measures.AirPressure_mb  = typecast(Reply(5:8), 'single')/100;
-# Measures.RelativeHumidity  = typecast(Reply(9:12), 'single');
-# BpodSystem.StopModuleRelay;
 
 my_bpod.close()
