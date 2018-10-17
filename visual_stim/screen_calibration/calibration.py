2b2832b0b146de6afba1bce48aef8afca400b9d3
Unknown <nbonacchi@gmail.com>
2018-10-17 16:40:18 +0100

    ..
    Great success!!
    Almost there, decide how it will run...
    Expose COM port and color? Save one file per color + all
    calibration.py has to run in the background maybe rename file to stream_frame2TTl.p or something...
    main_calibration.py should call streamer and run Bonsai to save the file fro R, G, and B, and Gray! Then with the files saved somewhere (probably in the ibldata folder at top level?)

---
+++ visual_stim/screen_calibration/calibration.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+# @Author: Niccolò Bonacchi
+# @Date: Tuesday, October 16th 2018, 12:13:00 pm
+# @Last Modified by: Niccolò Bonacchi
+# @Last Modified time: 16-10-2018 12:13:25.2525
+import serial
+from pythonosc import udp_client
+import time
+
+# osc_client = udp_client.SimpleUDPClient('127.0.0.1', 6667)
+
+# frame2ttl = 'COM6'  # /dev/ttyACM1'
+# ser = serial.Serial(port=frame2ttl, baudrate=115200, timeout=1)
+# ser.write(b'S')  # Start the stream, stream rate 100Hz
+
+# i = 0
+# while True:
+#     d = ser.read(4)
+#     d = int.from_bytes(d, byteorder='little')
+#     osc_client.send_message("/d", d)
+#     i += 1
+#     if i == 99:
+#         osc_client.send_message("/i", 1)
+#         i = 0
+    
+
+    
+
+if __name__ == '__main__':
+    osc_client = udp_client.SimpleUDPClient('127.0.0.1', 6667)
+
+    frame2ttl = 'COM6'  # /dev/ttyACM1'
+    ser = serial.Serial(port=frame2ttl, baudrate=115200, timeout=1)
+    ser.write(b'S')  # Start the stream, stream rate 100Hz
+    ser.write(b'S')  # Start the stream, stream rate 100Hz
+
+    i = 0
+    while True:
+        d = ser.read(4)
+        d = int.from_bytes(d, byteorder='little')
+        osc_client.send_message("/d", d)
+        i += 1
+        if i == 100:
+            osc_client.send_message("/i", i)
+            i = 0
+        
+            
+        print(i, d)
+    print('Done!')
