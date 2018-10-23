2a919cc337f0c2b35734d29c53d85c8037ff5520
Niccolò Bonacchi <nbonacchi@gmail.com>
2018-10-23 16:52:36 +0100

    Started changing the install file

--- install.py
+++ install.py
@@ -10,36 +10,35 @@ import json
 import subprocess
 import re
 import sys
+from pathlib import Path
 
 # Constants assuming Windows
-IBLRIG_ROOT_PATH = os.getcwd()
-PYBPOD_PATH = os.path.join(IBLRIG_ROOT_PATH, 'pybpod')
+IBLRIG_ROOT_PATH = Path.cwd()
+ENV_FILE = IBLRIG_ROOT_PATH / 'requirements.txt'
+# PYBPOD_PATH = os.path.join(IBLRIG_ROOT_PATH, 'pybpod')
 
 
-def get_pybpod_env(conda):
-    # Find environment
-    ENVS = subprocess.check_output([conda, "env", "list", "--json"])
-    ENVS = json.loads(ENVS.decode('utf-8'))
-    pat = re.compile("^.+pybpod-environment$")
-    PYBPOD_ENV = [x for x in ENVS['envs'] if pat.match(x)]
-    PYBPOD_ENV = PYBPOD_ENV[0] if PYBPOD_ENV else None
-    return PYBPOD_ENV
+# def get_pybpod_env(conda):
+#     # Find environment
+#     ENVS = subprocess.check_output([conda, "env", "list", "--json"])
+#     ENVS = json.loads(ENVS.decode('utf-8'))
+#     pat = re.compile("^.+pybpod-environment$")
+#     PYBPOD_ENV = [x for x in ENVS['envs'] if pat.match(x)]
+#     PYBPOD_ENV = PYBPOD_ENV[0] if PYBPOD_ENV else None
+#     return PYBPOD_ENV
 
 
-BASE_ENV_FILE = 'environment-{}.yml'
+# BASE_ENV_FILE = 'environment-{}.yml'
 if sys.platform in ['Windows', 'windows', 'win32']:
-    ENV_FILE = BASE_ENV_FILE.format('windows-10')
     CONDA = "conda"
     SITE_PACKAGES = os.path.join("lib", "site-packages")
 elif sys.platform in ['Linux', 'linux']:
-    ENV_FILE = BASE_ENV_FILE.format('ubuntu-17.10')
     p = sys.prefix.split(os.sep)
     p = [x for x in p if 'env' not in x]
     conda_path = '{}'.format(os.sep).join(p)
     CONDA = os.path.join(conda_path, "bin", "conda")
     SITE_PACKAGES = os.path.join("lib", "python3.6", "site-packages")
 elif sys.platform in ['Darwin', 'macOSx', 'osx']:
-    ENV_FILE = BASE_ENV_FILE.format('macOSx')
     print("ERROR: macOSx is not supported yet\nInstallation aborted!")
 else:
     print('\nERROR: Unsupported OS\nInstallation aborted!')
@@ -81,26 +80,26 @@ def check_dependencies():
     print("All dependencies OK.")
 
 
-def check_pybpod_for_initialization():
-    print('\n\nINFO: Checking pybpod for initialization:')
-    print("N" * 79)
-    os.chdir(IBLRIG_ROOT_PATH)
-    if not os.listdir(PYBPOD_PATH):
-        subprocess.call(["git", "submodule", "update", "--init",
-                         "--recursive"])
-    print("N" * 79)
-    print("PyBpod initialized.")
+# def check_pybpod_for_initialization():
+#     print('\n\nINFO: Checking pybpod for initialization:')
+#     print("N" * 79)
+#     os.chdir(IBLRIG_ROOT_PATH)
+#     if not os.listdir(PYBPOD_PATH):
+#         subprocess.call(["git", "submodule", "update", "--init",
+#                          "--recursive"])
+#     print("N" * 79)
+#     print("PyBpod initialized.")
 
 
-def clone_water_calibration_plugin():
-    print('\n\nINFO: Cloning water-claibration-plugin:')
-    print("N" * 79)
-    os.chdir(os.path.join(PYBPOD_PATH, 'plugins'))
-    subprocess.call(["git", "clone",
-                     'https://bitbucket.org/azi92rach/water-calibration-plugin.git'])
-    os.chdir(IBLRIG_ROOT_PATH)
-    print("N" * 79)
-    print("PyBpod initialized and water-calibration-plugin cloned.")
+# def clone_water_calibration_plugin():
+#     print('\n\nINFO: Cloning water-claibration-plugin:')
+#     print("N" * 79)
+#     os.chdir(os.path.join(PYBPOD_PATH, 'plugins'))
+#     subprocess.call(["git", "clone",
+#                      'https://bitbucket.org/azi92rach/water-calibration-plugin.git'])
+#     os.chdir(IBLRIG_ROOT_PATH)
+#     print("N" * 79)
+#     print("PyBpod initialized and water-calibration-plugin cloned.")
 
 
 def install_environment():
@@ -156,38 +155,38 @@ def install_extra_deps():
     print("IBL specific dependencies installed.")
 
 
-def install_pybpod():
-    print('\n\nINFO: Installing pybpod:')
-    print("N" * 79)
-    if PYBPOD_ENV is None:
-        msg = "Can't install pybpod, pybpod-environment not found"
-        raise ValueError(msg)
-    # Install pybpod
-    os.chdir(PYBPOD_PATH)
-    subprocess.call([PYTHON, "utils/install.py"])
-    os.chdir(IBLRIG_ROOT_PATH)
-    print("N" * 79)
-    print("INFO: PyBpod installed.")
+# def install_pybpod():
+#     print('\n\nINFO: Installing pybpod:')
+#     print("N" * 79)
+#     if PYBPOD_ENV is None:
+#         msg = "Can't install pybpod, pybpod-environment not found"
+#         raise ValueError(msg)
+#     # Install pybpod
+#     os.chdir(PYBPOD_PATH)
+#     subprocess.call([PYTHON, "utils/install.py"])
+#     os.chdir(IBLRIG_ROOT_PATH)
+#     print("N" * 79)
+#     print("INFO: PyBpod installed.")
 
 
-def install_water_calibration_plugin():
-    print('\n\nINFO: Installing water-calibration-plugins:')
-    print("N" * 79)
-    os.chdir(os.path.join(PYBPOD_PATH, 'plugins'))
-    subprocess.call([PIP, "install", "-e", "water-calibration-plugin"])
-    os.chdir(PYBPOD_PATH)
-    print("N" * 79)
-    print("water-calibration-plugin installed.")
+# def install_water_calibration_plugin():
+#     print('\n\nINFO: Installing water-calibration-plugins:')
+#     print("N" * 79)
+#     os.chdir(os.path.join(PYBPOD_PATH, 'plugins'))
+#     subprocess.call([PIP, "install", "-e", "water-calibration-plugin"])
+#     os.chdir(PYBPOD_PATH)
+#     print("N" * 79)
+#     print("water-calibration-plugin installed.")
 
 
-def conf_pybpod_settings():
-    print('\n\nINFO: Configuring pybpod IBL project:')
-    print("N" * 79)
-    # Copy user settings
-    src = os.path.join(IBLRIG_ROOT_PATH, 'user_settings.py')
-    shutil.copy(src, PYBPOD_PATH)
-    print("N" * 79)
-    print("Configuration complete.")
+# def conf_pybpod_settings():
+#     print('\n\nINFO: Configuring pybpod IBL project:')
+#     print("N" * 79)
+#     # Copy user settings
+#     src = os.path.join(IBLRIG_ROOT_PATH, 'user_settings.py')
+#     shutil.copy(src, PYBPOD_PATH)
+#     print("N" * 79)
+#     print("Configuration complete.")
 
 
 def install_bonsai():
@@ -206,8 +205,8 @@ def install_bonsai():
 if __name__ == '__main__':
     try:
         check_dependencies()
-        check_pybpod_for_initialization()
-        clone_water_calibration_plugin()
+        # check_pybpod_for_initialization()
+        # clone_water_calibration_plugin()
         install_environment()
         print("\n\n")
         PYBPOD_ENV, PIP, PYTHON_FILE, PYTHON = get_env_constants()
@@ -225,31 +224,4 @@ if __name__ == '__main__':
     except IOError as msg:
         print(msg, "\n\nSOMETHING IS WRONG: Bad! Bad install file!")
 
-# Install notes to be integrated in install procedure!
-# https: // bitbucket.org/fchampalimaud/downloads/downloads/
-# DL runtime and soundcard
 
-# Cables need to be inserted for installation
-# Install Runtime
-# next next ...
-
-# REBOOT
-
-# Uncheck windows fast bla bla from sound card installation
-
-# on desktop double click Sound card CF
-
-# Click here
-# open driver folder
-# doubleclick on installer
-# install driver
-
-
-# LAunch Sound generator and uploader
-
-# create sounds
-# i2 and i3
-
-# create sound and send to board... might request to install .NET3.5. just go ahead
-
-# (base) C: \Users\User\Documents\HarpSoundBoard\SoundUploader > HarpSoundCard.exe i4_white_noise_gaussian 4 0 96
