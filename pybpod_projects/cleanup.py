ff125afb4f902f700e9e44970bfce45b71d5bbb7
Unknown <nbonacchi@gmail.com>
2018-10-30 14:53:37 +0000

    Moved cleanup.py to projects folder updated post commands of basic task

--- pybpod_projects/cleanup.py
+++ pybpod_projects/cleanup.py
@@ -1,14 +1,19 @@
 """
 Removes pybpod data files from setup folders.
 Data and settings from pybpod data files is in ibl data files.
+Assumes it's called as a post command from task folder
 """
 from pathlib import Path
 import shutil
 
 p = Path.cwd()
+print('\n', p, '\n')
+print('\n', p.parent.parent.parent, '\n')
 
-sess_folders = p.rglob('sessions')
+sess_folders = p.parent.parent.parent.rglob('sessions')
+print('\n', sess_folders, '\n')
 
 for s in sess_folders:
+    print('\n', s, '\n')
     if 'setups' in str(s):
         shutil.rmtree(s)
