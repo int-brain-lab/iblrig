5a2c4a9010baf598ef2fdbdb4a706b980762e246
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-01-16 17:53:21 +0000

    init of misc scripts (daily_data_transfer.py)

---
+++ misc/daily_data_transfer.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+# @Author: Niccolò Bonacchi
+# @Date: Wednesday, January 16th 2019, 2:03:59 pm
+# @Last Modified by: Niccolò Bonacchi
+# @Last Modified time: 16-01-2019 02:04:01.011
+import datetime
+import shutil
+import sys
+from pathlib import Path
+
+
+def main(local_folder, remote_folder):
+    today = datetime.datetime.now().date().isoformat()
+    local_folder = Path(local_folder) / "Subjects"
+    remote_folder = Path(remote_folder) / "Subjects"
+
+    # Get all date folders of today
+    todays_sessions = []
+    for subject in local_folder.glob("*"):
+        todays_sessions.extend([x for x in subject.glob("*") if today in x.name])
+
+    # Get all session paths for today
+    all_sessions = []
+    for session in todays_sessions:
+        all_sessions.extend(session.glob("*"))
+
+    # Find all sessions that have "transfer_me.flag"
+    src_session_paths = []
+    for s in all_sessions:
+        src_session_paths.extend(
+            [s for x in s.glob("*") if "transfer_me.flag" in x.name])
+
+    # Create all dst paths
+    dst_session_paths = []
+    for s in src_session_paths:
+        mouse = s.parts[-3]
+        date = s.parts[-2]
+        sess = s.parts[-1]
+        d = remote_folder / mouse / date / sess
+        dst_session_paths.append(d)
+
+    for src, dst in zip(src_session_paths, dst_session_paths):
+        shutil.copytree(src, dst)
+
+
+if __name__ == "__main__":
+    # local_folder = "/home/nico/Projects/IBL/IBL-github/iblrig/scratch/test_iblrig_data"
+    # remote_folder = "/home/nico/Projects/IBL/IBL-github/iblrig/scratch/test_iblrig_data_on_server"
+    main(sys.argv[1], sys.argv[2])
