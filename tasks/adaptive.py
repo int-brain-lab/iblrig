c623c9915b27f0130a4328a711d96281ba306550
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-02-07 12:17:14 +0000

    TrainingCW: Impulsive control implementation

--- tasks/adaptive.py
+++ tasks/adaptive.py
@@ -6,6 +6,7 @@
 import logging
 import numpy as np
 import pandas as pd
+import scipy as sp
 import scipy.interpolate
 import ibllib.io.raw_data_loaders as raw
 
@@ -63,12 +64,12 @@ def init_calib_func_range(sph_obj) -> tuple:
     min_open_time = 0
     max_open_time = 1000
     msg = f"""
-    ##########################################
-    NOT FOUND: WATER RANGE CALIBRATION FILE
-    ##########################################
-            File might be missing or empty
-            range set to (0, 1000)ms
-    ##########################################"""
+        ##########################################
+        NOT FOUND: WATER RANGE CALIBRATION FILE
+        ##########################################
+                File might be missing or empty
+                range set to (0, 1000)ms
+        ##########################################"""
 
     if sph_obj.LATEST_WATER_CALIB_RANGE_FILE:
         # Load last calibration r ange df1
@@ -149,24 +150,37 @@ def load_settings(previous_session_path):
 
 
 def impulsive_control(sph_obj):
-    crit_1 = True  # 50% perf on one side ~100% on other
-    crit_2 = True  # Median RT on hard (<50%) contrasts < 300ms
-    crit_3 = True  # Getting enough water
-
+    crit_1 = False  # 50% perf on one side ~100% on other
+    crit_2 = False  # Median RT on hard (<50%) contrasts < 300ms
+    crit_3 = False  # Getting enough water
     data = raw.load_data(sph_obj.PREVIOUS_SESSION_PATH)
+    if data is None or not data:
+        return sph_obj
 
-    trial_side =
-    trial_correct =
-    trial_contrast =
-    rt =
+    signed_contrast = np.array([x['signed_contrast'] for x in data])
+    trial_correct = np.array([x['trial_correct'] for x in data])
 
+    # Check crit 1
+    l_trial_correct = trial_correct[signed_contrast < 0]
+    r_trial_correct = trial_correct[signed_contrast > 0]
+    p_left = sum(l_trial_correct) / len(l_trial_correct)
+    p_righ = sum(r_trial_correct) / len(r_trial_correct)
+    if np.abs(p_left - p_righ) >= 0.4:
+        crit_1 = True
+
+    # Check crit 2
+    rt = np.array([
+        x['behavior_data']['States timestamps']['closed_loop'][0][1] -
+        x['behavior_data']['States timestamps']['stim_on'][0][0] for x in data]
+    )
+    if sp.median(rt[np.abs(signed_contrast) < 0.5]) < 0.3:
+        crit_2 = True
     # Check crit 3
     previous_weight_factor = sph_obj.LAST_SETTINGS_DATA['SUBJECT_WEIGHT'] / 25
     previous_water = sph_obj.LAST_TRIAL_DATA['water_delivered'] / 1000
 
-    if previous_water < previous_weight_factor:
-        crit_3 = False
-
+    if previous_water >= previous_weight_factor:
+        crit_3 = True
 
     if crit_1 and crit_2 and crit_3:
         # Reward decrease
@@ -177,3 +191,12 @@ def impulsive_control(sph_obj):
         sph_obj.ITI_ERROR = 3.
         # Introduce interactive delay
         sph_obj.INTERACTIVE_DELAY = 0.250  # sec
+        sph_obj.IMPULSIVE_CONTROL = 'ON'
+
+    return sph_obj
+
+
+if __name__ == "__main__":
+    sess_path = ('/home/nico/Projects/IBL/IBL-github/iblrig' +
+                 '/scratch/test_iblrig_data/Subjects/ZM_335/2018-12-13/001')
+    data = raw.load_data(sess_path)
