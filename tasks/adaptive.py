36f548ed13eda549ce50487fe98462541b94e99c
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-02-08 21:10:28 +0000

    refactored sph_obj to sph

--- tasks/adaptive.py
+++ tasks/adaptive.py
@@ -13,37 +13,37 @@ import ibllib.io.raw_data_loaders as raw
 log = logging.getLogger('iblrig')
 
 
-def init_reward_amount(sph_obj):
-    if not sph_obj.ADAPTIVE_REWARD:
-        return sph_obj.REWARD_AMOUNT
+def init_reward_amount(sph) -> float:
+    if not sph.ADAPTIVE_REWARD:
+        return sph.REWARD_AMOUNT
 
-    if sph_obj.LAST_TRIAL_DATA is None:
-        return sph_obj.AR_INIT_VALUE
-    elif sph_obj.LAST_TRIAL_DATA and sph_obj.LAST_TRIAL_DATA['trial_num'] < 200:  # noqa
-        out = sph_obj.LAST_TRIAL_DATA['reward_amount']
-    elif sph_obj.LAST_TRIAL_DATA and sph_obj.LAST_TRIAL_DATA['trial_num'] >= 200:  # noqa
-        out = sph_obj.LAST_TRIAL_DATA['reward_amount'] - sph_obj.AR_STEP
-        out = sph_obj.AR_MIN_VALUE if out <= sph_obj.AR_MIN_VALUE else out
+    if sph.LAST_TRIAL_DATA is None:
+        return sph.AR_INIT_VALUE
+    elif sph.LAST_TRIAL_DATA and sph.LAST_TRIAL_DATA['trial_num'] < 200:  # noqa
+        out = sph.LAST_TRIAL_DATA['reward_amount']
+    elif sph.LAST_TRIAL_DATA and sph.LAST_TRIAL_DATA['trial_num'] >= 200:  # noqa
+        out = sph.LAST_TRIAL_DATA['reward_amount'] - sph.AR_STEP
+        out = sph.AR_MIN_VALUE if out <= sph.AR_MIN_VALUE else out
 
-    if 'SUBJECT_WEIGHT' not in sph_obj.LAST_SETTINGS_DATA.keys():
+    if 'SUBJECT_WEIGHT' not in sph.LAST_SETTINGS_DATA.keys():
         return out
 
-    previous_weight_factor = sph_obj.LAST_SETTINGS_DATA['SUBJECT_WEIGHT'] / 25
-    previous_water = sph_obj.LAST_TRIAL_DATA['water_delivered'] / 1000
+    previous_weight_factor = sph.LAST_SETTINGS_DATA['SUBJECT_WEIGHT'] / 25
+    previous_water = sph.LAST_TRIAL_DATA['water_delivered'] / 1000
 
     if previous_water < previous_weight_factor:
-        out = sph_obj.LAST_TRIAL_DATA['reward_amount'] + sph_obj.AR_STEP
+        out = sph.LAST_TRIAL_DATA['reward_amount'] + sph.AR_STEP
 
     return out
 
 
-def init_calib_func(sph_obj):
-    if not sph_obj.AUTOMATIC_CALIBRATION:
+def init_calib_func(sph):
+    if not sph.AUTOMATIC_CALIBRATION:
         return
 
-    if sph_obj.LATEST_WATER_CALIBRATION_FILE:
+    if sph.LATEST_WATER_CALIBRATION_FILE:
         # Load last calibration df1
-        df1 = pd.read_csv(sph_obj.LATEST_WATER_CALIBRATION_FILE)
+        df1 = pd.read_csv(sph.LATEST_WATER_CALIBRATION_FILE)
         # make interp func
         if df1.empty:
             msg = f"""
@@ -59,7 +59,7 @@ def init_calib_func(sph_obj):
         return
 
 
-def init_calib_func_range(sph_obj) -> tuple:
+def init_calib_func_range(sph) -> tuple:
 
     min_open_time = 0
     max_open_time = 1000
@@ -71,9 +71,9 @@ def init_calib_func_range(sph_obj) -> tuple:
                  range set to (0, 1000)ms
         ##########################################"""
 
-    if sph_obj.LATEST_WATER_CALIB_RANGE_FILE:
+    if sph.LATEST_WATER_CALIB_RANGE_FILE:
         # Load last calibration r ange df1
-        df1 = pd.read_csv(sph_obj.LATEST_WATER_CALIB_RANGE_FILE)
+        df1 = pd.read_csv(sph.LATEST_WATER_CALIB_RANGE_FILE)
         if not df1.empty:
             min_open_time = df1['min_open_time']
             max_open_time = df1['max_open_time']
@@ -83,24 +83,24 @@ def init_calib_func_range(sph_obj) -> tuple:
     return min_open_time, max_open_time
 
 
-def init_reward_valve_time(sph_obj):
+def init_reward_valve_time(sph) -> float:
     # Calc reward valve time
-    if not sph_obj.AUTOMATIC_CALIBRATION:
-        out = sph_obj.CALIBRATION_VALUE / 3 * sph_obj.REWARD_AMOUNT
-    elif sph_obj.AUTOMATIC_CALIBRATION and sph_obj.CALIB_FUNC is not None:
-        out = sph_obj.CALIB_FUNC_RANGE[0]
-        while np.round(sph_obj.CALIB_FUNC(out), 3) < sph_obj.REWARD_AMOUNT:
+    if not sph.AUTOMATIC_CALIBRATION:
+        out = sph.CALIBRATION_VALUE / 3 * sph.REWARD_AMOUNT
+    elif sph.AUTOMATIC_CALIBRATION and sph.CALIB_FUNC is not None:
+        out = sph.CALIB_FUNC_RANGE[0]
+        while np.round(sph.CALIB_FUNC(out), 3) < sph.REWARD_AMOUNT:
             out += 1
-            if out >= sph_obj.CALIB_FUNC_RANGE[1]:
+            if out >= sph.CALIB_FUNC_RANGE[1]:
                 break
         out /= 1000
-    elif sph_obj.AUTOMATIC_CALIBRATION and sph_obj.CALIB_FUNC is None:
+    elif sph.AUTOMATIC_CALIBRATION and sph.CALIB_FUNC is None:
         msg = """
         ##########################################
                 NO CALIBRATION FILE WAS FOUND:
         Calibrate the rig or use a manual calibration
         PLEASE GO TO:
-        iblrig_params/IBL/tasks/{sph_obj.PYBPOD_PROTOCOL}/task_settings.py
+        iblrig_params/IBL/tasks/{sph.PYBPOD_PROTOCOL}/task_settings.py
         and set:
             AUTOMATIC_CALIBRATION = False
             CALIBRATION_VALUE = <MANUAL_CALIBRATION>
@@ -115,7 +115,7 @@ def init_reward_valve_time(sph_obj):
         Probably because of a BAD calibration file
         Calibrate the rig or use a manual calibration
         PLEASE GO TO:
-        iblrig_params/IBL/tasks/{sph_obj.PYBPOD_PROTOCOL}/task_settings.py
+        iblrig_params/IBL/tasks/{sph.PYBPOD_PROTOCOL}/task_settings.py
         and set:
             AUTOMATIC_CALIBRATION = False
             CALIBRATION_VALUE = <MANUAL_CALIBRATION>
@@ -126,25 +126,25 @@ def init_reward_valve_time(sph_obj):
     return float(out)
 
 
-def init_stim_gain(sph_obj):
-    if not sph_obj.ADAPTIVE_GAIN:
-        return sph_obj.STIM_GAIN
+def init_stim_gain(sph) -> float:
+    if not sph.ADAPTIVE_GAIN:
+        return sph.STIM_GAIN
 
-    if sph_obj.LAST_TRIAL_DATA and sph_obj.LAST_TRIAL_DATA['trial_num'] >= 200:
-        stim_gain = sph_obj.AG_MIN_VALUE
+    if sph.LAST_TRIAL_DATA and sph.LAST_TRIAL_DATA['trial_num'] >= 200:
+        stim_gain = sph.AG_MIN_VALUE
     else:
-        stim_gain = sph_obj.AG_INIT_VALUE
+        stim_gain = sph.AG_INIT_VALUE
 
     return stim_gain
 
 
-def impulsive_control(sph_obj):
+def impulsive_control(sph):
     crit_1 = False  # 50% perf on one side ~100% on other
     crit_2 = False  # Median RT on hard (<50%) contrasts < 300ms
     crit_3 = False  # Getting enough water
-    data = raw.load_data(sph_obj.PREVIOUS_SESSION_PATH)
+    data = raw.load_data(sph.PREVIOUS_SESSION_PATH)
     if data is None or not data:
-        return sph_obj
+        return sph
 
     signed_contrast = np.array([x['signed_contrast'] for x in data])
     trial_correct = np.array([x['trial_correct'] for x in data])
@@ -165,24 +165,24 @@ def impulsive_control(sph_obj):
     if sp.median(rt[np.abs(signed_contrast) < 0.5]) < 0.3:
         crit_2 = True
     # Check crit 3
-    previous_weight_factor = sph_obj.LAST_SETTINGS_DATA['SUBJECT_WEIGHT'] / 25
-    previous_water = sph_obj.LAST_TRIAL_DATA['water_delivered'] / 1000
+    previous_weight_factor = sph.LAST_SETTINGS_DATA['SUBJECT_WEIGHT'] / 25
+    previous_water = sph.LAST_TRIAL_DATA['water_delivered'] / 1000
 
     if previous_water >= previous_weight_factor:
         crit_3 = True
 
     if crit_1 and crit_2 and crit_3:
         # Reward decrease
-        sph_obj.REWARD_AMOUNT -= sph_obj.AR_STEP  # 0.1 µl
-        if sph_obj.REWARD_AMOUNT < sph_obj.AR_MIN_VALUE:
-            sph_obj.REWARD_AMOUNT = sph_obj.AR_MIN_VALUE
+        sph.REWARD_AMOUNT -= sph.AR_STEP  # 0.1 µl
+        if sph.REWARD_AMOUNT < sph.AR_MIN_VALUE:
+            sph.REWARD_AMOUNT = sph.AR_MIN_VALUE
         # Increase timeout error
-        sph_obj.ITI_ERROR = 3.
+        sph.ITI_ERROR = 3.
         # Introduce interactive delay
-        sph_obj.INTERACTIVE_DELAY = 0.250  # sec
-        sph_obj.IMPULSIVE_CONTROL = 'ON'
+        sph.INTERACTIVE_DELAY = 0.250  # sec
+        sph.IMPULSIVE_CONTROL = 'ON'
 
-    return sph_obj
+    return sph
 
 
 if __name__ == "__main__":
