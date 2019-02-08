3dafae6d47840e62f9633c2f290d62ea18b7fd62
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-02-08 21:09:56 +0000

    added trial methods for RT checking to misc

--- tasks/misc.py
+++ tasks/misc.py
@@ -4,6 +4,7 @@
 # @Last Modified by: Niccolò Bonacchi
 # @Last Modified time: 8-02-2019 12:51:53.5353
 import numpy as np
+import datetime
 
 
 def get_port_events(events: dict, name: str = '') -> list:
@@ -35,7 +36,7 @@ def texp(factor: float = 0.35, min_: float = 0.2, max_: float = 0.5) -> float:
             return texp(factor=factor, min_=min_, max_=max_)
 
 
-def draw_contrast(contrast_set, prob_type='biased'):
+def draw_contrast(contrast_set, prob_type='biased') -> float:
     if prob_type == 'biased':
         # p = [1/(n-1 + 0.5)] * (n - 1)
         n_1 = len(contrast_set) - 1
@@ -45,3 +46,28 @@ def draw_contrast(contrast_set, prob_type='biased'):
         return np.random.choice(contrast_set, p=p)
     elif prob_type == 'uniform':
         return np.random.choice(contrast_set)
+
+
+def check_stop_criterions(init_datetime, rt_buffer, trial_num) -> int:
+        # STOPPING CRITERIONS
+        # < than 400 trials in 45 minutes
+        time_up = init_datetime + datetime.timedelta(minutes=45)
+        if time_up <= datetime.datetime.now() and trial_num <= 400:
+            return 1
+
+        # Median response time of latest N = 20 trials > than 5 times
+        # the median response time and more than 400 trials performed
+        N, T = 20, 400
+        if len(rt_buffer) >= N and trial_num > T:
+            latest_median = np.median(rt_buffer[-N:])
+            all_median = np.median(rt_buffer)
+
+            if latest_median > all_median * 5:
+                return 2
+
+        return False
+
+
+def get_trial_rt(behavior_data):
+    return (behavior_data['States timestamps']['closed_loop'][0][1] -
+            behavior_data['States timestamps']['stim_on'][0][0])
