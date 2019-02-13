0bbdf21f744d32956da47aaea2106da144d8b143
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-02-13 16:12:19 +0000

    added test in __main__ to calibration

--- tasks/_iblrig_calibration_water/session_params.py
+++ tasks/_iblrig_calibration_water/session_params.py
@@ -145,3 +145,33 @@ class SessionParamHandler(object):
         for dir in dir_list:
             SessionParamHandler.zipdir(dir, zipf)
         zipf.close()
+
+
+if __name__ == "__main__":
+    import task_settings as _task_settings
+    import scratch.calibration._user_settings as _user_settings
+    import datetime
+    from sys import platform
+    dt = datetime.datetime.now()
+    dt = [str(dt.year), str(dt.month), str(dt.day),
+          str(dt.hour), str(dt.minute), str(dt.second)]
+    dt = [x if int(x) >= 10 else '0' + x for x in dt]
+    dt.insert(3, '-')
+    _user_settings.PYBPOD_SESSION = ''.join(dt)
+    _user_settings.PYBPOD_SETUP = 'water'
+    _user_settings.PYBPOD_PROTOCOL = '_iblrig_calibration_water'
+    if platform == 'linux':
+        r = "/home/nico/Projects/IBL/IBL-github/iblrig"
+        _task_settings.IBLRIG_FOLDER = r
+        d = ("/home/nico/Projects/IBL/IBL-github/iblrig/scratch/" +
+             "test_iblrig_data")
+        _task_settings.IBLRIG_DATA_FOLDER = d
+        _task_settings.AUTOMATIC_CALIBRATION = False
+        _task_settings.USE_VISUAL_STIMULUS = False
+
+    sph = SessionParamHandler(_task_settings, _user_settings)
+    for k in sph.__dict__:
+        if sph.__dict__[k] is None:
+            print(f"{k}: {sph.__dict__[k]}")
+    self = sph
+    print("Done!")
