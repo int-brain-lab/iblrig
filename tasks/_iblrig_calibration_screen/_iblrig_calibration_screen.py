1bdab4c6d52264dcb748e6514f7dbf795e997a0d
N <nbonacchi@gmail.com>
2019-06-06 11:35:59 +0100

    calibration screen bpod task

---
+++ tasks/_iblrig_calibration_screen/_iblrig_calibration_screen.py
@@ -0,0 +1,5 @@
+import task_settings
+import user_settings  # PyBpod creates this file on run.
+from session_params import SessionParamHandler
+
+sph = SessionParamHandler(task_settings, user_settings)
