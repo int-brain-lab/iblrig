722f3e724837013a5a89bdf80b4f06a1e30f698d
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-06-14 16:53:56 +0100

    F2TTL bugfixes plus suggest method

--- visual_stim/F2TTL/F2TTL.py
+++ visual_stim/F2TTL/F2TTL.py
@@ -27,12 +27,16 @@ class frame2TTL(object):
         """Measure <num_samples> values from the sensor and return basic stats.
         Mean, Std, SEM, Nsamples
         """
+        import time
         sample_sum = []
         for i in range(num_samples):
             sample_sum.append(self.read_value())
+            time.sleep(0.001)
 
         out = {
             'mean_value': np.array(sample_sum).mean(),
+            'max_value': np.array(sample_sum).max(),
+            'min_value': np.array(sample_sum).min(),
             'std_value': np.array(sample_sum).std(),
             'sem_value':  np.array(sample_sum).std() / np.sqrt(num_samples),
             'nsamples': num_samples
@@ -47,11 +51,11 @@ class frame2TTL(object):
             dark = self.dark_threshold
 
         self.ser.write(b'C')
-        response = int.from_bytes(self.ser.read(1), byteorder='little')
-        if response != 218:
+        response = self.ser.read(1)
+        if response[0] != 218:
             raise(ConnectionError)
 
-        self.ser.write(struct.pack('cII', b'T', light, dark))
+        self.ser.write(struct.pack('<BHH', ord('T'), light, dark))
         if light != self.light_threshold:
             print(f"Light threshold set to {light}")
         if dark != self.dark_threshold:
@@ -62,6 +66,36 @@ class frame2TTL(object):
         self.dark_threshold = dark
         return
 
+    def suggest_thresholds(self):
+        reply = input(
+            "Set pixels under Frame2TTL to white (rgb 255,255,255) and press enter >")
+        print(" ")
+        print("Measuring white...")
+        lightData = self.measure_light(10000)
+
+        reply = input(
+            "Set pixels under Frame2TTL to black (rgb 0,0,0) and press enter >")
+        print(" ")
+        print("Measuring black...")
+        darkData = self.measure_light(10000)
+        print(" ")
+        lightMax = lightData.get('max_value')
+        darkMin = darkData.get('min_value')
+        print(
+            f"Max sensor reading for white (lower is brighter) = {lightMax}.")
+        print(f"Min sensor reading for black = {darkMin}.")
+        recommendedLightThreshold = lightMax
+        if darkMin - recommendedLightThreshold > 40:
+            recommendedDarkThreshold = recommendedLightThreshold + 40
+        else:
+            recommendedDarkThreshold = round(
+                recommendedLightThreshold + ((darkMin - recommendedLightThreshold)/3))
+        if recommendedDarkThreshold - recommendedLightThreshold < 5:
+            print("Error: Cannot recommend thresholds; light and dark measurements may be too close for accurate frame detection")
+        else:
+            print(
+                f"Recommended thresholds: Light = {recommendedLightThreshold}, Dark = {recommendedDarkThreshold}.")
+
     def read_value(self):
         """Read one value from sensor (current)"""
         self.ser.write(b'V')
@@ -70,10 +104,15 @@ class frame2TTL(object):
         response = int.from_bytes(response, byteorder='little')
         return response
 
-    def stream(self):
+    def start_stream(self):
         """Enable streaming to USB"""
-        self.ser.write(b'S')  # Start the stream, stream rate 100Hz
-        self.streaming = not self.streaming
+        self.ser.write(struct.pack('cB', b'S', 1))
+        self.streaming = True
+
+    def stop_stream(self):
+        """Disable streaming to USB"""
+        self.ser.write(struct.pack('cB', b'S', 0))
+        self.streaming = False
 
         # while self.streaming:
         #     response = int.from_bytes(self.ser.read(4), byteorder='little')
@@ -82,7 +121,7 @@ class frame2TTL(object):
 
 
 if __name__ == "__main__":
-    com_port = '/dev/ttyACM1'
+    com_port = '/dev/ttyACM0'
     f = frame2TTL(com_port)
     print(f.read_value())
     print(f.measure_light())
