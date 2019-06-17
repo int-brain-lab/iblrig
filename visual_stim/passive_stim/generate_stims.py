eab50b43c46e455f83bcdb7c4e23186cb135e845
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-06-17 14:56:32 +0100

    generate_stims_script for passive stims

---
+++ visual_stim/passive_stim/generate_stims.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Niccolò Bonacchi
+# @Date: Monday, June 17th 2019, 2:06:00 pm
+"""
+Six contrasts are presented (100; 50; 25; 12; 6; 4 %). Each grating is
+presented (ON) for 2 s,  with a fixed time interval between gratings (OFF) of
+1 s during which the gray background is presented. Each grating is presented 20
+times, at each left-right location, in a randomised order.
+"""
+import numpy as np
+
+
+CONTRASTS = [1.0, 0.5, 0.25, 0.125, 0.0625, 0.04]
+POSITIONS = [-35, 35]
+NREPEAT = 20
+
+pos = sorted(POSITIONS * len(CONTRASTS) * NREPEAT)
+cont = CONTRASTS * NREPEAT * 2
+
+data = np.array([[int(p), c] for p, c in zip(pos, cont)])
+
+np.random.shuffle(data)
+data = np.array([[str(int(p)), str(c)] for p, c in data])
+np.savetxt('stims.csv', data, delimiter=' ', fmt='%s')
+
