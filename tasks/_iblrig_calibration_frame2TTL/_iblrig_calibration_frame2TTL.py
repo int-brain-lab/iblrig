dd6a91f31e4a07bf0fa67498cccf14f4c0312f2d
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-07-08 18:35:45 +0100

    F2TTL calibration: update and upload to alyx TODO: use Bonsai on screen

--- tasks/_iblrig_calibration_frame2TTL/_iblrig_calibration_frame2TTL.py
+++ tasks/_iblrig_calibration_frame2TTL/_iblrig_calibration_frame2TTL.py
@@ -8,24 +8,20 @@ task_settings.IBLRIG_FOLDER = Path(__file__).parent.parent.parent
 
 sph = SessionParamHandler(task_settings, user_settings)
 
-
-server = Path(sph.IBLRIG_FOLDER) / 'visual_stim' / \
-    'screen_calibration' / 'photodiode_server.py'
-# Start the frame2TTL server
 sph.f2ttl.suggest_thresholds()
-sph.alyx.update_board_params()
+sph.update_board_params()
 
-bns = Path(sph.IBLRIG_FOLDER) / 'Bonsai' / 'Bonsai64.exe'
-wrkfl = Path(sph.IBLRIG_FOLDER) / 'visual_stim' / \
-    'screen_calibration' / 'screen_sweep.bonsai'
-noedit = '--no-editor'  # implies start
-nodebug = '--start-no-debug'
-start = '--start'
-editor = nodebug
-# Properties
-save = '-p:Save=True'
-fname = '-p:FileName={sph}_iblrig_calibration_screen_brightness.raw.ssv'
-rgb = '-p:RGB=RGB'
+# bns = Path(sph.IBLRIG_FOLDER) / 'Bonsai' / 'Bonsai64.exe'
+# wrkfl = Path(sph.IBLRIG_FOLDER) / 'visual_stim' / \
+#     'screen_calibration' / 'screen_sweep.bonsai'
+# noedit = '--no-editor'  # implies start
+# nodebug = '--start-no-debug'
+# start = '--start'
+# editor = nodebug
+# # Properties
+# save = '-p:Save=True'
+# fname = '-p:FileName={sph}_iblrig_calibration_screen_brightness.raw.ssv'
+# rgb = '-p:RGB=RGB'
 
 # cmd = [bns, wrkfl, editor, save, fname, rgb]
 # s = subprocess.call(cmd, stdout=subprocess.PIPE)
