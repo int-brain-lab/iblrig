12a2c898f964655fb1d8fad5821b02db2a375ca8
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-08-13 16:49:27 +0100

    Bumped up version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='iblrig',
-    version='5.2.7',
+    version='5.2.8',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
     description='IBL libraries',
     license="MIT",
