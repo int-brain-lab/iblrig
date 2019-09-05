bdba3d2ee308523096c78b5caf662359b61debe4
Niccolo Bonacchi <nbonacchi@gmail.com>
2019-09-05 23:16:07 +0100

    created pfc file

---
+++ iblrig/pfc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Niccolò Bonacchi
+# @Date: Wednesday, September 4th 2019, 4:24:59 pm
+"""Pre flight checklist
+Define task, user, subject and board
+Check Alyx connection
+    Alyx present:
+       Load COM ports
+       Load calibration data water func & frame2TTL
+       Set frame2TTL thresholds
+       Create folders
+       Run task
+    Alyx absent:
+
+    Load COM ports
+end with user input
+"""
