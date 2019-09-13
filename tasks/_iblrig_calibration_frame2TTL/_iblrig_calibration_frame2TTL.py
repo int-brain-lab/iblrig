b5ac52da2dc273f08f52bc911d98530e948c2262
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-09-13 19:02:04 +0100

    calibration_f2ttl update file before updating Alyx

--- tasks/_iblrig_calibration_frame2TTL/_iblrig_calibration_frame2TTL.py
+++ tasks/_iblrig_calibration_frame2TTL/_iblrig_calibration_frame2TTL.py
@@ -1,10 +1,13 @@
+import json
 import logging
 import time
+import datetime
 
 import iblrig.alyx as alyx
 import task_settings
 import user_settings
-from iblrig.frame2TTL import Frame2TTL
+from iblrig.frame2TTL import Frame2TTL, update_frame2ttl_params_file
+from iblrig.path_helper import get_iblrig_params_folder
 from session_params import SessionParamHandler
 
 log = logging.getLogger('iblrig')
@@ -23,11 +26,18 @@ resp = f2ttl.calc_recomend_thresholds()
 if resp != -1:
     f2ttl.set_recommendations()
 
-    patch = {'F2TTL_COM': f2ttl.serial_port,
+    patch = {'COM_F2TTL': f2ttl.serial_port,
              'F2TTL_DARK_THRESH': f2ttl.recomend_dark,
-             'F2TTL_LIGHT_THRESH': f2ttl.recomend_light}
+             'F2TTL_LIGHT_THRESH': f2ttl.recomend_light,
+             'F2TTL_CALIBRATION_DATE': datetime.datetime.now().isoformat()}
 
-    alyx.update_board_params(sph.PYBPOD_BOARD, patch)  # XXX: Save local copy
+    iblrig_params_folder = get_iblrig_params_folder()
+    update_frame2ttl_params_file(data=patch)
+    try:
+        alyx.update_board_params(data=patch)
+    except Exception as e:
+        log.error(e)
+        log.error("Unable to sync to Alyx, data was saved locally.")
 
 sph.stop_screen_color()
 
