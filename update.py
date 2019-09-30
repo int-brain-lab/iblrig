07aafffa55743c221c399a47fbffe34af09bc2e7
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-09-30 15:05:40 +0100

    Restricted available versions

--- update.py
+++ update.py
@@ -44,7 +44,7 @@ def get_versions():
                                     "--tags", "origin"]).decode().split()
     vers = [x for x in vers[1::2] if '{' not in x]
     vers = [x.split('/')[-1] for x in vers]
-    available = [x for x in vers if x >= '5.0.0']
+    available = [x for x in vers if x >= '5.3.1']
     print("Available versions: {}".format(available))
     return vers
 
