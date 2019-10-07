21f2210133a4b03ca492b50fc7a507fdcc32183a
N <nbonacchi@gmail.com>
2019-10-07 15:01:52 +0100

    Added call to ODS count

--- tasks/_iblrig_tasks_ephys_certification/_iblrig_tasks_ephys_certification.py
+++ tasks/_iblrig_tasks_ephys_certification/_iblrig_tasks_ephys_certification.py
@@ -24,19 +24,22 @@ wrkfl = str(certification_folder / 'ephys_certification.bonsai')
 # Flags
 noedit = '--no-editor'  # implies start and no-debug?
 noboot = '--no-boot'
+start = '--start'
 # Properties
 SA0_DueTime = '-p:SpontaneousActivity0.DueTime=00:15:00'
-SA1_DueTime = '-p:SpontaneousActivity1.DueTime=00:15:00'
+ODS0_Count = '-p:OrientationDirectionSelectivityStim0.Count=20'
 RFM_FileName = '-p:ReceptiveFieldMappingStim.FileNameRFMapStim=' + str(
-    Path(sph.SESSION_RAW_DATA_FOLDER) / '_iblrig_RFMapStim.raw.bin')
+    Path(SESSION_RAW_DATA_FOLDER) / '_iblrig_RFMapStim.raw.bin')
 RFM_MappingTime = '-p:ReceptiveFieldMappingStim.MappingTime=00:10:00'
 CRCS_CheckerboardTime = '-p:ContrastReversingCheckerboardStim.CheckerboardTime=00:03:00'
 CSTS_StimFileName = '-p:ContrastSelectivityTaskStim.StimFileName=' + str(
     certification_folder/ 'Extensions' / 'stims.csv')
-
-cmd = [bns, wrkfl, noedit, noboot, SA0_DueTime, SA1_DueTime,
-        RFM_FileName, RFM_MappingTime, CRCS_CheckerboardTime, CSTS_StimFileName]
-
+SA1_DueTime = '-p:SpontaneousActivity1.DueTime=00:15:00'
+ODS1_Count = '-p:OrientationDirectionSelectivityStim1.Count=20'
+# Build command
+cmd = [bns, wrkfl, noboot, noedit, SA0_DueTime, SA1_DueTime, RFM_FileName, ODS0_Count, ODS1_Count
+       RFM_MappingTime, CRCS_CheckerboardTime, CSTS_StimFileName, ]
+# Run visual stims
 os.chdir(certification_folder)
 s = subprocess.run(cmd, stdout=subprocess.PIPE)  # locking call
 os.chdir(CWD)
