b03387859d31eb25770a049642292bb1f95a0ce4
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-11-14 18:31:11 +0000

    ...

--- tasks/_iblrig_tasks_passiveChoiceWorld/session_params.py
+++ tasks/_iblrig_tasks_passiveChoiceWorld/session_params.py
@@ -32,9 +32,7 @@ class SessionParamHandler(object):
 
     def __init__(self, task_settings, user_settings, debug=False, fmake=True):
         self.DEBUG = debug
-        make = True
-        self.IBLRIG_FOLDER = 'C:\\iblrig'
-        self.IBLRIG_DATA_FOLDER = None  # ..\\iblrig_data if None
+        make = False
         # =====================================================================
         # IMPORT task_settings, user_settings, and SessionPathCreator params
         # =====================================================================
@@ -293,7 +291,7 @@ if __name__ == '__main__':
         turning off lights of bpod board
     """
     import task_settings as _task_settings
-    import scratch._user_settings as _user_settings
+    import iblrig.fake_user_settings as _user_settings
     import datetime
     dt = datetime.datetime.now()
     dt = [str(dt.year), str(dt.month), str(dt.day),
@@ -301,14 +299,9 @@ if __name__ == '__main__':
     dt = [x if int(x) >= 10 else '0' + x for x in dt]
     dt.insert(3, '-')
     _user_settings.PYBPOD_SESSION = ''.join(dt)
-    _user_settings.PYBPOD_SETUP = 'biasedChoiceWorld'
-    _user_settings.PYBPOD_PROTOCOL = '_iblrig_tasks_biasedChoiceWorld'
+    _user_settings.PYBPOD_SETUP = 'passiveChoiceWorld'
+    _user_settings.PYBPOD_PROTOCOL = '_iblrig_tasks_passiveChoiceWorld'
     if platform == 'linux':
-        r = "/home/nico/Projects/IBL/github/iblrig"
-        _task_settings.IBLRIG_FOLDER = r
-        d = ("/home/nico/Projects/IBL/github/iblrig/scratch/" +
-             "test_iblrig_data")
-        _task_settings.IBLRIG_DATA_FOLDER = d
         _task_settings.AUTOMATIC_CALIBRATION = False
         _task_settings.USE_VISUAL_STIMULUS = False
 
