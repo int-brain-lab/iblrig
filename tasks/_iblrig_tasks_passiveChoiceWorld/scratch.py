910cb3aa07531a00c48d7cc8d0c9eabb625b5d82
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-11-20 17:11:37 +0000

    New logic

--- tasks/_iblrig_tasks_passiveChoiceWorld/scratch.py
+++ tasks/_iblrig_tasks_passiveChoiceWorld/scratch.py
@@ -37,35 +37,52 @@ n_labels = ['N'] * 40
 t_labels = ['T'] * 40
 v_labels = ['V'] * 40
 
-g_delay_dist = np.random.uniform(0.500, 1.900, len(g_labels))
-n_delay_dist = np.random.uniform(1, 5, len(n_labels))
-t_delay_dist = np.random.uniform(1, 5, len(t_labels))
-v_delay_dist = np.random.uniform(1, 11, len(v_labels))
 
-g_delays = np.cumsum(g_delay_dist) + g_len
-n_delays = np.cumsum(n_delay_dist) + n_len
-t_delays = np.cumsum(t_delay_dist) + t_len
-v_delays = np.cumsum(v_delay_dist) + v_len
+def guniform(min_, max_, len_):
+    for x in np.random.uniform(min_, max_, len_):
+        yield x
 
 
-sess_delays = np.concatenate([g_delays, n_delays, t_delays, v_delays])
-sess_labels = np.array(g_labels + n_labels + t_labels + v_labels)
-sess_lens = np.concatenate([g_len, n_len, t_len, v_len])
+delay_dists = {
+    'G': guniform(0.500, 1.900, len(g_labels)),
+    'N': guniform(1, 5, len(n_labels)),
+    'T': guniform(1, 5, len(t_labels)),
+    'V': guniform(1, 11, len(v_labels))
+}
+stim_delays = np.random.uniform(0.25, 0.85, 300)
+stim_labels = np.array(g_labels + n_labels + t_labels + v_labels)
+np.random.shuffle(stim_labels)
+for i, (d, l) in enumerate(zip(stim_delays, stim_labels)):
 
-# Sort acording to the delays
-srtd_idx = np.argsort(sess_delays)
+    if i == 0:
+        pass
+    else:
+        if stim_labels[i - 1] == stim_labels[i]:
+            stim_delays[i] = next(delay_dists[l])
 
-sess_delays = sess_delays[srtd_idx]
-sess_labels = sess_labels[srtd_idx]
-sess_lens = sess_lens[srtd_idx]
-# Add previous stim duration to delay
-# temp_delays = sess_delays[:-1] + sess_lens[1:]
-# sess_delays = np.insert(temp_delays, 0, sess_delays[0])
+np.cumsum(stim_delays)
 
-# Check if the min diff is reasonable
-mindiff = np.min(np.diff(sess_delays))
-amin = np.argmin(np.diff(sess_delays))
+# sess_delays = np.concatenate([g_delays, n_delays, t_delays, v_delays])
+# sess_delay_dists = np.concatenate([g_delay_dist, n_delay_dist, t_delay_dist, v_delay_dist])
+# sess_labels = np.array(g_labels + n_labels + t_labels + v_labels)
+# sess_lens = np.concatenate([g_len, n_len, t_len, v_len])
 
-print(sess_labels[amin - 1:amin + 2])
-print(sess_delays[amin - 1:amin + 2])
-print(mindiff)
+# # Sort acording to the delays
+# srtd_idx = np.argsort(sess_delays)
+
+# sess_delays = sess_delays[srtd_idx]
+# sess_labels = sess_labels[srtd_idx]
+# sess_lens = sess_lens[srtd_idx]
+# # Add previous stim duration to delay
+# # temp_delays = sess_delays[:-1] + sess_lens[1:]
+# # sess_delays = np.insert(temp_delays, 0, sess_delays[0])
+
+# # Check if the min diff is reasonable
+# mindiff = np.min(np.diff(sess_delays))
+# amin = np.argmin(np.diff(sess_delays))
+
+# print(sess_labels[amin - 1:amin + 2])
+# print(sess_delays[amin - 1:amin + 2])
+# print(mindiff)
+# plt.plot(np.diff(sess_delays), 'o')
+# plt.show()
