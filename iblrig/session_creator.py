bd9bb769ae698f1b2055958c1c396f07e973c873
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-11-21 17:30:30 +0000

    Refactored session_creator / removed generate_passive_script

--- iblrig/session_creator.py
+++ iblrig/session_creator.py
@@ -3,7 +3,6 @@
 # @Author: Niccolò Bonacchi
 # @Date: Thursday, March 28th 2019, 7:19:15 pm
 import math
-import random
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -15,8 +14,13 @@ import iblrig.misc as misc
 import iblrig.path_helper as ph
 
 
-# TODO: Refactor for ephysCW and passiveCW, rename to ephys_session_file_creator?
-def make_pc():
+# EPHYS CHOICE WORLD
+def make_ephysCW_pc():
+    """make_ephysCW_pc Makes positions, contrasts and block lengths for ephysCW
+        Generates ~2000 trias
+    :return: pc
+    :rtype: [type]
+    """
     contrasts = [1., 0.25, 0.125, 0.0625, 0.0]
     len_block = [90]
     pos = [-35] * int(len_block[0] / 2) + [35] * int(len_block[0] / 2)
@@ -37,12 +41,12 @@ def make_pc():
     return pc, len_block
 
 
-def make_pcqs(pc):
+def make_ephysCW_pcqs(pc):
     qperiod_base = 0.2  # + x, where x~exp(0.35), t ∈ 0.2 <= R <= 0.5
     sphase = []
     qperiod = []
     for i in pc:
-        sphase.append(random.uniform(0, math.pi))
+        sphase.append(np.random.uniform(0, math.pi))
         qperiod.append(
             qperiod_base + misc.texp(factor=0.35, min_=0.2, max_=0.5))
     qs = np.array([qperiod, sphase]).T
@@ -50,13 +54,14 @@ def make_pcqs(pc):
     return pcqs
 
 
-def generate_sessions(nsessions, path='./tasks/_iblrig_tasks_ephysChoiceWorld/sessions'):
+def pre_generate_ephysCW_session_files(
+        nsessions, path='./tasks/_iblrig_tasks_ephysChoiceWorld/sessions'):
     iblrig_path = Path(ph.get_iblrig_folder())
     path = iblrig_path / Path(path)
     path.mkdir(parents=True, exist_ok=True)
     for i in range(nsessions):
-        pc, len_block = make_pc()
-        pcqs = make_pcqs(pc)
+        pc, len_block = make_ephysCW_pc()
+        pcqs = make_ephysCW_pcqs(pc)
         np.save(path / f'pcqs_session_{i}.npy', pcqs)
         np.save(path / f'pcqs_session_{i}_len_blocks.npy', len_block)
 
@@ -101,16 +106,132 @@ def plot_pcqs(session_num, folder='./tasks/_iblrig_tasks_ephysChoiceWorld/sessio
     return pcqs, len_block
 
 
+# CERTIFICATION
+def make_stims_for_certification_pcs(seed_num=None, save=False):
+    if seed_num is not None:
+        np.random.seed(seed_num)
+    iblrig_path = Path(ph.get_iblrig_folder())
+    # Generate the position and contrast for the replayed stims
+    contrasts = [1.0, 0.5, 0.25, 0.125, 0.0625]
+
+    positions = [-35, 35]
+    pc_repeats = 20
+
+    pos = sorted(positions * len(contrasts) * pc_repeats)
+    cont = contrasts * pc_repeats * len(positions)
+
+    sphase = [np.random.uniform(0, math.pi) for x in cont]
+    gabors = np.array([[int(p), c, s] for p, c, s in zip(pos, cont, sphase)])
+
+    np.random.shuffle(gabors)
+    # Make into strings for saving
+    if save:
+        fpath = iblrig_path / 'visual_stim' / 'ephys_certification'
+        fpath = fpath / 'Extensions' / 'certification_stims.csv'
+        np.savetxt(fpath, gabors, delimiter=' ', fmt=['%d', '%f', '%f'])
+
+    return gabors
+
+
+# PASSIVE CHOICE WORLD
+def make_stims_for_passiveCW_pcs(seed_num=None, save=False):
+    if seed_num is not None:
+        np.random.seed(seed_num)
+    iblrig_path = Path(ph.get_iblrig_folder())
+    # Generate the position and contrast for the replayed stims
+    contrasts = [1.0, 0.25, 0.125, 0.0625]
+    zero_contrasts = [0.0]
+
+    positions = [-35, 35]
+    pc_repeats = 20
+    # zero % contrast is added with half the amount of pc_repeats
+    zero_repeats = len(positions) * pc_repeats / 2
+
+    pos = sorted(positions * len(contrasts) * pc_repeats)
+    cont = contrasts * pc_repeats * len(positions)
+
+    pos.extend(positions * int(zero_repeats / len(positions)))
+    cont.extend(zero_contrasts * int(zero_repeats))
+    sphase = [np.random.uniform(0, math.pi) for x in cont]
+    gabors = np.array([[int(p), c, s] for p, c, s in zip(pos, cont, sphase)])
+
+    np.random.shuffle(gabors)
+    # Make into strings for saving
+    if save:
+        fpath = iblrig_path / 'visual_stim' / 'passiveChoiceWorld'
+        fpath = fpath / 'Extensions' / 'passiveCW_stims.csv'
+        np.savetxt(fpath, gabors, delimiter=' ', fmt=['%d', '%f', '%f'])
+
+    return gabors
+
+
+def make_passiveCW_session_delays_ids(seed_num=None):
+    if seed_num is not None:
+        np.random.seed(seed_num)
+
+    g_len = np.ones((180)) * 0.3
+    n_len = np.ones((40)) * 0.5
+    t_len = np.ones((40)) * 0.1
+    v_len = np.ones((40)) * 0.2
+
+    g_labels = ['G'] * 180
+    n_labels = ['N'] * 40
+    t_labels = ['T'] * 40
+    v_labels = ['V'] * 40
+
+    g_delay_dist = np.random.uniform(0.500, 1.900, len(g_labels))
+    n_delay_dist = np.random.uniform(1, 5, len(n_labels))
+    t_delay_dist = np.random.uniform(1, 5, len(t_labels))
+    v_delay_dist = np.random.uniform(1, 11, len(v_labels))
+
+    # Calculate when they all should happen
+    sess_delays_cumsum = np.concatenate([
+        np.cumsum(g_delay_dist),
+        np.cumsum(n_delay_dist),
+        np.cumsum(t_delay_dist),
+        np.cumsum(v_delay_dist)
+    ])
+    sess_labels_out = np.array(g_labels + n_labels + t_labels + v_labels)
+
+    # Sort acording to the when they happen
+    srtd_idx = np.argsort(sess_delays_cumsum)
+    sess_delays_cumsum = sess_delays_cumsum[srtd_idx]
+    sess_labels_out = sess_labels_out[srtd_idx]
+    # get the delays between the stims
+    sess_delays_out = np.diff(sess_delays_cumsum)
+    tot_dur = np.sum(np.sum(g_len) + np.sum(n_len) + np.sum(t_len) +
+                     np.sum(v_len) + np.sum(sess_delays_out)) / 60
+
+    # print(f'Stim IDs: {sess_labels_out}')
+    # print(f'Stim delays: {sess_delays_out}')
+    print(f'Total duration of stims: {tot_dur} m')
+
+    return sess_delays_out, sess_labels_out
+
+
+def pre_generate_passiveCW_session_files(
+        nsessions, path='./tasks/_iblrig_tasks_ephysChoiceWorld/sessions'):
+    iblrig_path = Path(ph.get_iblrig_folder())
+    path = iblrig_path / Path(path)
+    path.mkdir(parents=True, exist_ok=True)
+    for i in range(nsessions):
+        delays, ids, = make_passiveCW_session_delays_ids()
+        np.save(path / f'session_{i}_passive_stimDelays.npy', delays)
+        np.save(path / f'session_{i}_passive_stimIDs.npy', ids)
+
+
 if __name__ == "__main__":
     # import seaborn as sns
-    plt.ion()
+    # plt.ion()
     # pcqs3, len_block3 = plot_pcqs(3)
-    pcqs9, len_block9 = plot_pcqs(9)
+    # pcqs9, len_block9 = plot_pcqs(9)
     # sns.distplot(pcqs3[:, 2], vertical=True)
     # sns.jointplot(x=range(len(pcqs9)), y=pcqs9[:, 1])
     # qp = sns.jointplot(x=range(len(pcqs9)),
     #                    y=pcqs9[:, 2], kind='kde', figsize=(16, 12), dpi=80)
     # qp.set_axis_labels(xlabel='Trials', ylabel='Quiescent period (s)')
-    # generate_sessions(1, path='sess')
+    # pre_generate_ephysCW_session_filess(1, path='sess')
     # plot_pcqs(0, folder='sess')
+    # gabors = make_stims_for_passiveCW_pcs(save=True)
+    pre_generate_passiveCW_session_files(12)
     print('.')
--- tasks/_iblrig_tasks_passiveChoiceWorld/scratch.py
+++ tasks/_iblrig_tasks_passiveChoiceWorld/scratch.py
@@ -2,61 +2,87 @@
 # -*- coding:utf-8 -*-
 # @Author: Niccolò Bonacchi
 # @Date: Friday, November 15th 2019, 12:05:29 pm
+import math
+
 import matplotlib.pyplot as plt
 import numpy as np
+
 from iblrig.path_helper import get_iblrig_folder
 
-np.random.seed(42)
 
-IBLRIG_FOLDER = get_iblrig_folder()
-# Generate the position and contrast for the replayed stims
-CONTRASTS = [1.0, 0.25, 0.125, 0.0625]
-ZERO_CONTRASTS = [0.0]
+def make_passiveCW_pcs(seed_num, save=False):
+    np.random.seed(seed_num)
+    IBLRIG_FOLDER = get_iblrig_folder()
+    # Generate the position and contrast for the replayed stims
+    contrasts = [1.0, 0.25, 0.125, 0.0625]
+    zero_contrasts = [0.0]
 
-POSITIONS = [-35, 35]
-NREPEAT = 20
+    positions = [-35, 35]
+    pc_repeats = 20
+    # zero % contrast is added with half the amount of pc_repeats
+    zero_repeats = len(positions) * pc_repeats / 2
 
-pos = sorted(POSITIONS * len(CONTRASTS) * NREPEAT)
-cont = CONTRASTS * NREPEAT * 2
-pos.extend(POSITIONS * 10)
-cont.extend(ZERO_CONTRASTS * 20)
-gabors = np.array([[int(p), c] for p, c in zip(pos, cont)])
+    pos = sorted(positions * len(contrasts) * pc_repeats)
+    cont = contrasts * pc_repeats * len(positions)
 
-np.random.shuffle(gabors)
-# Make into strings for saving
-# data = np.array([[str(int(p)), str(c)] for p, c in data])
-# fpath = IBLRIG_FOLDER / 'visual_stim' / 'passive_stim' / 'stims.csv'
-# np.savetxt(fpath, data, delimiter=' ', fmt='#s')
-g_len = np.ones((180)) * 0.3
-n_len = np.ones((40)) * 0.5
-t_len = np.ones((40)) * 0.1
-v_len = np.ones((40)) * 0.2
+    pos.extend(positions * (zero_repeats / len(positions)))
+    cont.extend(zero_contrasts * zero_repeats)
+    sphase = [np.random.uniform(0, math.pi) for x in cont]
+    gabors = np.array([[int(p), c, s] for p, c, s in zip(pos, cont, sphase)])
 
-g_labels = ['G'] * 180
-n_labels = ['N'] * 40
-t_labels = ['T'] * 40
-v_labels = ['V'] * 40
+    np.random.shuffle(gabors)
+    # Make into strings for saving
+    if save:
+        data = np.array([[str(int(p)), str(c), str(s)] for p, c, s in gabors])
+    fpath = IBLRIG_FOLDER / 'visual_stim' / 'passiveChoiceWorld' / 'Extensions' / 'pcs_stims.csv'
+    np.savetxt(fpath, data, delimiter=' ', fmt='#s')
+    return gabors
 
 
-def guniform(min_, max_, len_):
-    for x in np.random.uniform(min_, max_, len_):
-        yield x
+def make_passiveCW_session_delays_id(seed_num, save_session=False):
+    np.random.seed(seed_num)
+
+    g_len = np.ones((180)) * 0.3
+    n_len = np.ones((40)) * 0.5
+    t_len = np.ones((40)) * 0.1
+    v_len = np.ones((40)) * 0.2
+
+    g_labels = ['G'] * 180
+    n_labels = ['N'] * 40
+    t_labels = ['T'] * 40
+    v_labels = ['V'] * 40
+
+    g_delay_dist = np.random.uniform(0.500, 1.900, len(g_labels))
+    n_delay_dist = np.random.uniform(1, 5, len(n_labels))
+    t_delay_dist = np.random.uniform(1, 5, len(t_labels))
+    v_delay_dist = np.random.uniform(1, 11, len(v_labels))
+
+    # Calculate when they all should happen
+    sess_delays_cumsum = np.concatenate([
+        np.cumsum(g_delay_dist),
+        np.cumsum(n_delay_dist),
+        np.cumsum(t_delay_dist),
+        np.cumsum(v_delay_dist)
+    ])
+    sess_labels_out = np.array(g_labels + n_labels + t_labels + v_labels)
+
+    # Sort acording to the when they happen
+    srtd_idx = np.argsort(sess_delays_cumsum)
+    sess_delays_cumsum = sess_delays_cumsum[srtd_idx]
+    sess_labels_out = sess_labels_out[srtd_idx]
+    # get the delays between the stims
+    sess_delays_out = np.diff(sess_delays_cumsum)
+    tot_dur = np.sum(np.sum(g_len) + np.sum(n_len) + np.sum(t_len) +
+                     np.sum(v_len) + np.sum(sess_delays_out)) / 60
+
+    print(f'Stim IDs: {sess_labels_out}')
+    print(f'Stim delays: {sess_delays_out}')
+    print(f'Total duration of stims: {tot_dur} m')
 
 
-delay_dists = {
-    'G': guniform(0.500, 1.900, len(g_labels)),
-    'N': guniform(1, 5, len(n_labels)),
-    'T': guniform(1, 5, len(t_labels)),
-    'V': guniform(1, 11, len(v_labels))
-}
-stim_delays = np.random.uniform(0.25, 0.85, 300)
-stim_labels = np.array(g_labels + n_labels + t_labels + v_labels)
-np.random.shuffle(stim_labels)
-for i, (d, l) in enumerate(zip(stim_delays, stim_labels)):
-    if i == 0:
-        pass
-    else:
-        if stim_labels[i - 1] == stim_labels[i]:
-            stim_delays[i] = next(delay_dists[l])
+def pre_generate_passiveCW_session_files():
+    pass
 
-np.cumsum(stim_delays)[-1] / 60
+if __name__ == "__main__":
+    make_passiveCW_session_delays_id(43)
+    print('.')
