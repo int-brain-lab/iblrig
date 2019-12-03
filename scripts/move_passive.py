7bf91550f1678e9779e267c052def1f16229e0a7
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-12-03 18:20:13 +0000

    Finished move passive

--- scripts/move_passive.py
+++ scripts/move_passive.py
@@ -8,23 +8,38 @@ from pathlib import Path
 import ibllib.io.raw_data_loaders as raw
 import ibllib.pipes.misc as misc
 import iblrig.path_helper as ph
+import logging
+
+log = logging.getLogger('iblrig')
 
 IBLRIG_DATA_PATH = Path(ph.get_iblrig_data_folder())
 
-ephys_sessions = IBLRIG_DATA_PATH.rglob("raw_passive_data_missing.flag")
-passive_sessions = IBLRIG_DATA_PATH.rglob("passive_data_for_ephys.flag")
 
-# For each passive session found look into passiveSettings to find ephysSession name
-# search for the ephys session session in the rglobbed ephys sessions
-# If you find it just rename and move the folder raw_behavior_data -> raw_passive_data,
-# If no find search specifically for that session from the metadata and try to copy the folder
-# If folder exists throw an error
-for ps in passive_sessions:
-    sett = raw.load_settings(str(ps.parent))
-    esess = sett['CORRESPONDING_EPHYS_SESSION']
-    # Fails if dst_folder exists!
-    misc.transfer_folder(
-        str(ps.parent / 'raw_behavior_data'),
-        str(Path(esess) / 'raw_passive_data'),
-        force=False
-    )
+def main():
+    passive_sessions = IBLRIG_DATA_PATH.rglob("passive_data_for_ephys.flag")
+
+    # For each passive session found look into passiveSettings to find ephysSession name
+    # search for the ephys session session in the rglobbed ephys sessions
+    # If you find it just rename and move the folder raw_behavior_data -> raw_passive_data,
+    # If no find search specifically for that session from the metadata and try to copy the folder
+    # If folder exists throw an error
+    for ps in passive_sessions:
+        sett = raw.load_settings(str(ps.parent))
+        esess = sett['CORRESPONDING_EPHYS_SESSION']
+        if not esess or esess is None:
+            log.warning("Corresponding ephys session NOT FOUND in settings - data not moved")
+            return
+        if not Path(esess).exists():
+            log.warning(f"Ephys session {esess}: NOT FOUND on filesystem - data not moved")
+            return
+        # Fails if dst_folder exists!
+        misc.transfer_folder(
+            str(ps.parent / 'raw_behavior data'),
+            str(Path(esess) / 'raw_passive_data'),
+            force=False
+        )
+        ps.unlink()
+
+
+if __name__ == "__main__":
+    main()
