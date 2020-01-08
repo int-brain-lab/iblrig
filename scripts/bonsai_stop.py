e5444bd2c23cd2ff1959c656104f70416b996ce7
Niccolò Bonacchi <nbonacchi@gmail.com>
2020-01-08 15:09:08 +0000

    TODO: check if this works!

--- scripts/bonsai_stop.py
+++ scripts/bonsai_stop.py
@@ -1,3 +1,5 @@
+# import iblrig.bonsai as bonsai
+# TODO: check if this works!
 from pythonosc import udp_client
 import sys
 
@@ -7,3 +9,10 @@ if __name__ == "__main__":
     OSC_CLIENT_PORT = int(sys.argv[1])
     osc_client = udp_client.SimpleUDPClient(OSC_CLIENT_IP, OSC_CLIENT_PORT)
     osc_client.send_message("/x", 1)
+
+    # stim = bonsai.osc_client('stim')
+    # camera = bonsai.osc_client('camera')
+    # mic = bonsai.osc_client('mic')
+    # stim.send_message("/x", 1)
+    # camera.send_message("/x", 1)
+    # mic.send_message("/x", 1)
