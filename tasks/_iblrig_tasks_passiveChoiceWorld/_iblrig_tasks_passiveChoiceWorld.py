2524bd3213e765c7a39fde4856579249119dcd47
Niccolo Bonacchi <nbonacchi@gmail.com>
2020-03-26 19:31:51 +0000

    Bugfix in passive

--- tasks/_iblrig_tasks_passiveChoiceWorld/_iblrig_tasks_passiveChoiceWorld.py
+++ tasks/_iblrig_tasks_passiveChoiceWorld/_iblrig_tasks_passiveChoiceWorld.py
@@ -37,10 +37,8 @@ re = MyRotaryEncoder(
 )
 sph.ROTARY_ENCODER = re
 
-# Turn bpod lights off
-bpod_lights(PARAMS["COM_BPOD"], 0)
 # get bpod
-bpod = Bpod(serial_port=PARAMS["COM_BPOD"])
+bpod = Bpod()
 # Build messages
 msg = BpodMessageCreator(bpod)
 sc_play_tone = msg.sound_card_play_idx(sph.GO_TONE_IDX)
