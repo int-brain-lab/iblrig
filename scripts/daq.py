31766748ad75bd90d4c041467c645dc429d8a37b
N <nbonacchi@gmail.com>
2021-03-11 11:21:21 +0000

    More daq prototyping

--- scripts/daq.py
+++ scripts/daq.py
@@ -4,18 +4,32 @@ import matplotlib.pyplot as plt
 import nidaqmx
 import numpy as np
 from nidaqmx.constants import AcquisitionType, Edge, TerminalConfiguration, VoltageUnits
+from pathlib import Path
+import numpy as np
+import os
 
 
-%matplotlib auto
-fig = plt.figure()
-ax1 = fig.add_subplot(211)
-ax2 = fig.add_subplot(212)
+# %matplotlib auto
+# fig = plt.figure()
+# ax1 = fig.add_subplot(211)
+# ax2 = fig.add_subplot(212)
 
-nfft = 1024
-fs = 1000
-freqs = np.linspace(0, int(fs / 2), int(nfft / 2 + 1))
+nsamples = 1024
+sample_frequency = 1000
+# freqs = np.linspace(0, int(sample_frequency / 2), int(nsamples / 2 + 1))
 
-task = nidaqmx.Task()
+# task = nidaqmx.Task()
+# with nidaqmx.Task() as task:
+#     task.di_channels.add_di_chan(
+#         "Dev1/port0/line0",
+#     )
+#     task.timing.cfg_samp_clk_timing(
+#         sample_frequency, active_edge=Edge.RISING, sample_mode=AcquisitionType.CONTINUOUS, samps_per_chan=nsamples
+#     )
+#     # task.start()
+#     # task.stop()
+
+
 with nidaqmx.Task() as task:
     task.ai_channels.add_ai_voltage_chan(
         "Dev1/ai0",
@@ -25,25 +39,44 @@ with nidaqmx.Task() as task:
         units=VoltageUnits.VOLTS,
     )
     task.timing.cfg_samp_clk_timing(
-        fs, active_edge=Edge.RISING, sample_mode=AcquisitionType.CONTINUOUS, samps_per_chan=nfft
+        sample_frequency,
+        active_edge=Edge.RISING,
+        sample_mode=AcquisitionType.CONTINUOUS,
+        samps_per_chan=1000
     )
-    # task.start()
-    # task.stop()
-    while True:
-        t1 = int(round(time.time() * 1000))
-        data = task.read(number_of_samples_per_channel=nfft)
-        t2 = int(round(time.time() * 1000))
-        tdata = np.array(data)
-        tdata -= np.mean(tdata)
-        ffto = np.fft.fft(tdata)
-        ffto = np.abs(ffto)
-        ffto = ffto[0 : (nfft / 2 + 1)]
 
-        ax1.clear()
-        ax2.clear()
-        ax1.plot(data)
-        ax2.plot(freqs, ffto)
-        fig.show(0)
-        plt.pause(0.000000001)
-        t3 = int(round(time.time() * 1000))
-        print(f"t2-t1 = {t2 - t1}ms, t3-t2 ={t3 - t2}ms")
+    fpath = Path(r"C:\iblrig_data\test.npy")
+    with fpath.open('ab') as f:
+        # task.start()
+        # task.stop()
+        tstart = time.time()
+        tend = time.time()
+        while tend - tstart < 4:
+            t1 = time.time()
+            data = task.read(number_of_samples_per_channel=4096)
+            t2 = tend = time.time()
+            tdata = np.array(data)
+            np.save(f, tdata)
+            # tdata -= np.mean(tdata)
+            # ffto = np.fft.fft(tdata)
+            # ffto = np.abs(ffto)
+            # ffto = ffto[0 : (int(nsamples / 2) + 1)]
+
+            # ax1.clear()
+            # ax2.clear()
+            # ax1.plot(data)
+            # ax2.plot(freqs, ffto)
+            # fig.show(0)
+            # plt.pause(0.000000001)
+            # t3 = int(round(time.time() * 1000))
+            print(f"t2-t1 = {t2 - t1}s")
+
+
+with fpath.open('rb') as f:
+    fsz = os.fstat(f.fileno()).st_size
+    out = np.load(f)
+    while f.tell() < fsz:
+        out = np.vstack((out, np.load(f)))
+
+fpath.unlink()
+print(out.shape)
