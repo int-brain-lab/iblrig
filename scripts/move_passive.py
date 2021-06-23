df2bc117b71476c2d6a4e09361108090862f327e
Niccolo Bonacchi <nbonacchi@gmail.com>
2021-06-23 12:33:45 +0100

    try catch in move_passive will move all possible sessions

--- scripts/move_passive.py
+++ scripts/move_passive.py
@@ -24,26 +24,29 @@ def main():
     # If folder exists throw an error
     log.info(f"Found {len(list(passive_sessions))} sessions in {IBLRIG_DATA_PATH}")
     for ps in passive_sessions:
-        sett = raw.load_settings(str(ps.parent))
-        esess = sett["CORRESPONDING_EPHYS_SESSION"]
-        if not esess or esess is None:
-            log.warning(
-                "Corresponding ephys session NOT FOUND in settings - data not moved"
+        try:
+            sett = raw.load_settings(str(ps.parent))
+            esess = sett["CORRESPONDING_EPHYS_SESSION"]
+            if not esess or esess is None:
+                log.warning(
+                    "Corresponding ephys session NOT FOUND in settings - data not moved"
+                )
+                return
+            if not Path(esess).exists():
+                log.warning(
+                    f"Ephys session {esess}: NOT FOUND on filesystem - data not moved"
+                )
+                return
+            # Fails if dst_folder exists!
+            misc.transfer_folder(
+                str(ps.parent / "raw_behavior_data"),
+                str(Path(esess) / "raw_passive_data"),
+                force=False,
             )
-            return
-        if not Path(esess).exists():
-            log.warning(
-                f"Ephys session {esess}: NOT FOUND on filesystem - data not moved"
-            )
-            return
-        # Fails if dst_folder exists!
-        misc.transfer_folder(
-            str(ps.parent / "raw_behavior_data"),
-            str(Path(esess) / "raw_passive_data"),
-            force=False,
-        )
-        log.info(f"Moved passive data to {esess}")
-        ps.unlink()
+            log.info(f"Moved passive data to {esess}")
+            ps.unlink()
+        except BaseException as e:
+            log.warning(f"{e}\n Failed to move passive session {ps.parent}")
 
 
 if __name__ == "__main__":
