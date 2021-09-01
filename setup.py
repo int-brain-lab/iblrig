b5c3422433078afdd92e12b087769e5af52f403a
Niccolo Bonacchi <nbonacchi@gmail.com>
2021-09-01 12:41:25 +0100

    Set dev version to 6.5.0dev

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="iblrig",
-    version="6.6.0dev",
+    version="6.5.0dev",
     python_requires=">={}.{}".format(*REQUIRED_PYTHON),
     description="IBL libraries",
     license="MIT",
