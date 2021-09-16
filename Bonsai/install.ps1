3e1d79bd7a5ed6bd22b27c3e9a6b77d2ddbbd0ab
N <nbonacchi@gmail.com>
2021-09-16 18:43:52 +0100

    Update bonsai bootstrapper to 2.6.3

--- Bonsai/install.ps1
+++ Bonsai/install.ps1
@@ -1,5 +1,5 @@
 if (!(Test-Path "./Bonsai.exe")) {
-    Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.6.2/Bonsai.zip" -OutFile "temp.zip"
+    Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.6.3/Bonsai.zip" -OutFile "temp.zip"
     Expand-Archive "temp.zip" -DestinationPath "." -Force
     Remove-Item -Path "temp.zip"
 }
