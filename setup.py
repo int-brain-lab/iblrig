417557ba1a327b12d6052c7b6b7154cd7a5fe3c0
N <nbonacchi@gmail.com>
2021-09-16 18:07:38 +0100

    removed dev referenc for version for RC data acquisition

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="iblrig",
-    version="6.5.0dev",
+    version="6.5.0",
     python_requires=">={}.{}".format(*REQUIRED_PYTHON),
     description="IBL libraries",
     license="MIT",
