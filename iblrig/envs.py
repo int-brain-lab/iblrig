02a4a7b7b214dd686fe0600d5e6a48a4f1abd051
N <nbonacchi@gmail.com>
2021-09-29 20:32:17 +0100

    Try CI bugfix of subprocess call on checkiing env names

--- iblrig/envs.py
+++ iblrig/envs.py
@@ -21,8 +21,15 @@ def get_env_folder(env_name: str = "iblenv") -> str:
     :return: folder path of conda environment
     :rtype: str
     """
-    all_envs = subprocess.check_output([f"{MC}", "env", "list", "--json"], shell=True)
-    all_envs = json.loads(all_envs.decode("utf-8"))
+    try:
+        all_envs = subprocess.check_output([f"{MC}", "env", "list", "--json"])
+        all_envs = json.loads(all_envs.decode("utf-8"))
+    except BaseException:
+        try:
+            all_envs = subprocess.check_output([f"{MC}", "env", "list", "--json"], shell=True)
+            all_envs = json.loads(all_envs.decode("utf-8"))
+        except BaseException:
+            raise ValueError("Could not run subprocess conda list environments")
     pat = re.compile(f"^.+{env_name}$")
     env = [x for x in all_envs["envs"] if pat.match(x)]
     env = env[0] if env else None
