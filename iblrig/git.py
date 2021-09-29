4e89251b563d47107e4c36898b946a3fa22b558a
N <nbonacchi@gmail.com>
2021-09-29 10:52:28 +0100

    git cleanup

--- iblrig/git.py
+++ iblrig/git.py
@@ -3,19 +3,19 @@
 # @File: iblrig/git.py
 # @Author: Niccolo' Bonacchi (@nbonacchi)
 # @Date: Friday, July 16th 2021, 12:57:29 pm
-import argparse
 import subprocess
+from packaging.version import parse
 
 
-def get_versions(verbose=False):
+def get_versions(verbose=False, only_available=False):
     vers = subprocess.check_output(["git", "ls-remote", "--tags", "origin"]).decode().split()
     vers = [x for x in vers[1::2] if "{" not in x]
     vers = [x.split("/")[-1] for x in vers]
     broken = ["6.3.0", "6.3.1"]
-    available = [x for x in vers if x >= "6.2.5" and x not in broken]
+    available = [x for x in vers if parse(x) >= parse("6.2.5") and x not in broken]
     if verbose:
         print("Available versions: {}".format(available))
-    return vers
+    return available if only_available else vers
 
 
 def get_branches(verbose=False):
@@ -84,38 +84,4 @@ def branch_info():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Checkout a specific version of the IBLRIG project. \
-        If no version is specified, checkout the current branch."
-    )
-    parser.add_argument(
-        "version",
-        nargs="?",
-        default=None,
-        help="The version to checkout. \
-        If no version is specified, checkout the current branch.",
-    )
-    parser.add_argument(
-        "--branch",
-        nargs="?",
-        default="master",
-        help="The branch to checkout. \
-        If no branch is specified, checkout the current branch.",
-    )
-    parser.add_argument(
-        "--file",
-        nargs="?",
-        default=None,
-        help="The file to checkout from the current branch. \
-        If no file is specified, checkout the current branch.",
-    )
-    args = parser.parse_args()
-
-    if args.version is not None:
-        checkout_version(args.version)
-    elif args.branch is not None:
-        checkout_branch(args.branch)
-    elif args.file is not None:
-        checkout_single_file(args.file, branch="master")
-    else:
-        checkout_single_file(branch="master")
+    pass
