177d9bc3d164313b39a9cea1f4b0080119d3cfb2
Niccolò <Bonacchi>
2021-11-02 16:01:06 +0000

    Avoid code copy on training and habituation on ephys rig

--- tasks/_iblrig_tasks_habituationChoiceWorld/session_params.py
+++ tasks/_iblrig_tasks_habituationChoiceWorld/session_params.py
@@ -114,8 +114,9 @@ class SessionParamHandler(object):
             iotasks.save_session_settings(self)
             iotasks.copy_task_code(self)
             iotasks.save_task_code(self)
-            iotasks.copy_video_code(self)
-            iotasks.save_video_code(self)
+            if "ephys" not in self.PYBPOD_BOARD:
+                iotasks.copy_video_code(self)
+                iotasks.save_video_code(self)
             self.bpod_lights(0)
 
     # =========================================================================
