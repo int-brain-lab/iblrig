3a5aaed0974f61561a7463e370b65fb450c57a81
Niccolo Bonacchi <nbonacchi@gmail.com>
2021-12-17 13:39:24 +0000

    bumped version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="iblrig",
-    version="6.5.3-canary",
+    version="6.5.3",
     python_requires=">={}.{}".format(*REQUIRED_PYTHON),
     description="IBL libraries",
     license="MIT",
