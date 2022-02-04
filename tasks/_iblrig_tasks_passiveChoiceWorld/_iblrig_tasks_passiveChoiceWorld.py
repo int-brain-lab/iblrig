3eaf94471fe118d7034b08a6ec87559ab7ef3a7e
Niccolo Bonacchi <nbonacchi@gmail.com>
2022-02-04 15:11:04 +0000

    sorted imports

--- tasks/_iblrig_tasks_passiveChoiceWorld/_iblrig_tasks_passiveChoiceWorld.py
+++ tasks/_iblrig_tasks_passiveChoiceWorld/_iblrig_tasks_passiveChoiceWorld.py
@@ -7,20 +7,20 @@ import logging
 import sys
 import time
 
-import numpy as np
-import usb
-import user_settings
-from pybpodapi.protocol import Bpod, StateMachine
-
 import iblrig.bonsai as bonsai
 import iblrig.frame2TTL as frame2TTL
 import iblrig.iotasks as iotasks
 import iblrig.misc as misc
 import iblrig.params as params
-import task_settings
+import numpy as np
+import usb
 from iblrig.bpod_helper import BpodMessageCreator, bpod_lights
 from iblrig.graphic import popup
 from iblrig.rotary_encoder import MyRotaryEncoder
+from pybpodapi.protocol import Bpod, StateMachine
+
+import task_settings
+import user_settings
 from session_params import SessionParamHandler
 
 log = logging.getLogger("iblrig")
