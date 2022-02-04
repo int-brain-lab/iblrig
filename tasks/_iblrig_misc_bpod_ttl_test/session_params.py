b1031d3a5d20f81286d02db9b908a5e3d665888e
Niccolo Bonacchi <nbonacchi@gmail.com>
2022-02-04 15:04:40 +0000

    Refactored and sorted importsimport

--- tasks/_iblrig_misc_bpod_ttl_test/session_params.py
+++ tasks/_iblrig_misc_bpod_ttl_test/session_params.py
@@ -6,16 +6,15 @@
 import logging
 from sys import platform
 
-import numpy as np
-from pythonosc import udp_client
-
 import iblrig.bonsai as bonsai
 import iblrig.frame2TTL as frame2TTL
 import iblrig.iotasks as iotasks
-import iblrig.raw_data_loaders as raw
 import iblrig.sound as sound
+import numpy as np
 from iblrig.path_helper import SessionPathCreator
+from iblrig.raw_data_loaders import load_data
 from iblrig.rotary_encoder import MyRotaryEncoder
+from pythonosc import udp_client
 
 log = logging.getLogger("iblrig")
 
@@ -114,7 +113,7 @@ class SessionParamHandler(object):
 
     def check_data(self):
         log.info("Checking...")
-        data = raw.load_data(self.SESSION_FOLDER)
+        data = load_data(self.SESSION_FOLDER)
         for t in data:
             bad_tone_state = t["behavior_data"]["States timestamps"]["bad_tone"]
             bad_stim_state = t["behavior_data"]["States timestamps"]["bad_stim"]
