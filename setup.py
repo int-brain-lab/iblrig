45a424ed04ff322487be70601eb51bc3c26d4217
Michele Fabbri <michele.fabbri@internationalbrainlab.org>
2022-03-14 11:11:06 +0000

    get_version added to setup.py

--- setup.py
+++ setup.py
@@ -22,9 +22,25 @@ with open("README.md", "r") as f:
 with open("requirements.txt") as f:
     require = [x.strip() for x in f.readlines()]
 
+
+def read(rel_path):
+    here = Path(__file__).parent.absolute()
+    with open(here.joinpath(rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name="iblrig",
-    version="6.5.3",
+    version=get_version(Path("iblrig").joinpath("__init__.py")),
     python_requires=">={}.{}".format(*REQUIRED_PYTHON),
     description="IBL libraries",
     license="MIT",
