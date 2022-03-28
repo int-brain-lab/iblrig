1b37721ea5d8d1770269c01abedd50bc4d3c056c
Niccolo Bonacchi <nbonacchi@gmail.com>
2022-03-28 16:49:35 +0100

    tear_down method for alyx_test

--- test_iblrig/test_alyx.py
+++ test_iblrig/test_alyx.py
@@ -1,4 +1,6 @@
 import unittest
+import subprocess
+import shutil
 
 from pathlib import Path
 import iblrig.ibllib_calls as calls
@@ -6,19 +8,21 @@ import iblrig.ibllib_calls as calls
 
 class TestIbllibCalls(unittest.TestCase):
     def setUp(self):
-        pass
+        self.project_name = "ibl_mainenlab"
+        self.one_test = True
 
     def test_call_one_get_project_data(self):
-        calls.call_one_get_project_data("ibl_mainenlab", one_test=True)
+        calls.call_one_get_project_data(self.project_name, one_test=self.one_test)
         self.assertTrue(Path().home().joinpath("TempAlyxProjectData").exists())
-        self.assertTrue(Path().home().joinpath("TempAlyxProjectData", "ibl_mainenlab_subjects.json").exists())
+        self.assertTrue(Path().home().joinpath("TempAlyxProjectData", f"{self.project_name}_subjects.json").exists())
 
     def test_call_one_sync_params(self):
-        resp = calls.call_one_sync_params(one_test=True)
-        self.assertTrue(not resp.stderr.decode())
+        resp = calls.call_one_sync_params(one_test=self.one_test)
+        self.assertTrue(isinstance(resp, subprocess.CompletedProcess))
+        self.assertTrue(resp.returncode == 0)
 
     def tearDown(self):
-        pass
+        shutil.rmtree(calls.ROOT_FOLDER, ignore_errors=True)
 
 
 if __name__ == "__main__":
