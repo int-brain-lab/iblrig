e8b72d15317654b0bce6f4d2139ba2fbac53c87d
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2022-08-30 16:47:03 +0100

    remove pulse at end of iti

--- tasks/_iblrig_tasks_habituationChoiceWorld/_iblrig_tasks_habituationChoiceWorld.py
+++ tasks/_iblrig_tasks_habituationChoiceWorld/_iblrig_tasks_habituationChoiceWorld.py
@@ -111,7 +111,7 @@ for i in range(sph.NTRIALS):  # Main loop
         state_name="iti",
         state_timer=tph.iti,
         state_change_conditions={"Tup": "exit"},
-        output_actions=[("BNC1", 255)],
+        output_actions=[],
     )
 
     # Send state machine description to Bpod device
