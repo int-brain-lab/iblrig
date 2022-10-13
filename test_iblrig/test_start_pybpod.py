e71d12e944f7c9e21229f6770027de34da8ac341
Michele Fabbri <michele.fabbri@internationalbrainlab.org>
2022-10-13 16:40:31 +0100

    test_start_pybpod cleanup

--- test_iblrig/test_start_pybpod.py
+++ test_iblrig/test_start_pybpod.py
@@ -1,30 +1,24 @@
 """
 Script used to test that pybpod launches with the given test data in the fixtures dir; ensure when running this script that the
-iblrig_params dir already exists and that the script is executed from that dir
+iblrig_params dir already exists and that the script is executed from that dir (required for pybpod functionality)
 """
 import argparse
-import os
 import shutil
 from pathlib import Path
+from sys import platform
+
+# Set iblrig_params path based on platform
+IBLRIG_PARAMS_PATH = Path("C:\\iblrig_params") if platform == "win32" else Path.home() / "Documents" / "iblrig_params"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Setup iblpybpod")
-    parser.add_argument("-t", "--test", action="store_true", help="Test mode")
+    parser.add_argument("-t", "--test", action="store_true", help="Removes local iblrig_params dir and copies test data")
     args = parser.parse_args()
 
+    iblrig_path = Path(__file__).parents[1]
     if args.test:  # Set vars for testing mode and copy files to appropriate dirs
-        iblrig_path = Path(__file__).parents[1]
-        iblrig_params_path = Path.home() / "Documents" / "iblrig_params"
-        shutil.copytree(iblrig_path / "test_iblrig" / "fixtures" / "test_iblrig_params", iblrig_params_path, dirs_exist_ok=True)
-    else:
-        iblrig_path = Path("C:\\iblrig")
-        iblrig_params_path = Path("C:\\iblrig_params")
-        os.makedirs(iblrig_params_path, exist_ok=True)  # Create params dir if it does not already exist
-
-        # Copy test pybpod user_settings.py from iblrig repo to params dir
-        src_file = iblrig_path / "scripts" / "user_settings.py"
-        dst_file = iblrig_params_path / "user_settings.py"
-        shutil.copy(src_file, dst_file) if not dst_file.exists() else None
+        shutil.rmtree(IBLRIG_PARAMS_PATH)
+        shutil.copytree(iblrig_path / "test_iblrig" / "fixtures" / "test_iblrig_params", IBLRIG_PARAMS_PATH, dirs_exist_ok=True)
 
     # start pybpod
     from pybpodgui_plugin.__main__ import start as start_pybpod
