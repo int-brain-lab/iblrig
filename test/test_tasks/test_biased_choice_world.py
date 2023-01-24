1500134baddd5f8b9fca9b872aa9d2cb13d2c5b8
Michele Fabbri <michele.fabbri@internationalbrainlab.org>
2023-01-24 15:38:26 +0000

    ci iblrig and bonsai install

--- test/test_tasks/test_biased_choice_world.py
+++ test/test_tasks/test_biased_choice_world.py
@@ -11,6 +11,8 @@ from iblrig_tasks._iblrig_tasks_neuroModulatorChoiceWorld.task import Session as
 class TestBiasedChoiceWorld(unittest.TestCase):
 
     def setUp(self) -> None:
+        # TODO: point to setting/blah_template files here
+        # TODO: sounddevice python package required 'sudo apt install libportaudio2' on ubuntu 22.04
         self.task = BiasedChoiceWorldSession(interactive=False)
 
     def test_task(self):
