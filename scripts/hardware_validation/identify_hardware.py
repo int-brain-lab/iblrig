44f69e3269c975f49ecde27d22931a19a64c30e4
Florian Rau <bimac@users.noreply.github.com>
2023-07-24 17:13:44 +0100

    Squashed commit of the following:
    commit a4a15c3317313f7eb4d6ec7ab78b7ba416c8437b
    Author: Florian Rau <bimac@users.noreply.github.com>
    Date:   Mon Jul 24 17:13:09 2023 +0100
    
        Update identify_hardware.py
    
    commit ec4d7552000d2d979bb41b1245cb153b76b258de
    Author: Olivier Winter <olivier.winter@internationalbrainlab.org>
    Date:   Mon Jul 24 17:11:36 2023 +0100
    
        Update identify_hardware.py
    
    commit ea388383a3d7c563685ef7b66317b596d42ccc56
    Author: Florian Rau <bimac@users.noreply.github.com>
    Date:   Mon Jul 24 12:16:50 2023 +0100
    
        Create identify_hardware.py

---
+++ scripts/hardware_validation/identify_hardware.py
@@ -0,0 +1,55 @@
+import sys
+import glob
+from serial import Serial, SerialException
+import array
+
+if sys.platform.startswith('win'):
+    ports = ['COM%s' % (i + 1) for i in range(256)]
+elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+    # this excludes your current terminal "/dev/tty"
+    ports = glob.glob('/dev/tty[A-Za-z]*')
+elif sys.platform.startswith('darwin'):
+    ports = glob.glob('/dev/tty.*')
+else:
+    raise EnvironmentError('Unsupported platform')
+
+
+def query(s_obj, req, n=1):
+    s_obj.write(req)
+    return s.read(n)
+
+
+for port in ports:
+    try:
+        s = Serial(port, timeout=0.1)
+        m = 'unknown'
+        v = None
+
+        for trial in range(4):
+            s.flush()
+            if trial == 0 and query(s, b'6') == b'5':
+                v = array.array('H', query(s, b'F', 4))
+                match(v[1]):
+                    case 1: hw = '0.5'
+                    case 2: hw = 'r0.7+'
+                    case 3: hw = 'r2.0-2.5'
+                    case 4: hw = '2+ r1.0'
+                    case _: hw = '(unknown version)'
+                m = 'Bpod {}, firmware {}'.format(hw, v[0])
+                s.write(b'Z')
+                break
+            elif trial == 1 and query(s, b'C') == (218).to_bytes(1, 'little'):
+                v = 2 if len(query(s, b'#')) > 0 else 1
+                m = 'frame2ttl, v{}'.format(v)
+                break
+            elif trial == 2 and len(query(s, b'Q', 2)) > 1 and query(s, b'P00', 1) == (1).to_bytes(1, 'little'):
+                v = '2+' if query(s, b'I') == (0).to_bytes(1, 'little') else 1
+                m = 'rotary encoder module, v{}'.format(v)
+                break
+            elif trial == 3:
+                pass
+
+        s.flush()
+        print('{}: {}'.format(s.portstr, m))
+    except (OSError, SerialException):
+        pass
