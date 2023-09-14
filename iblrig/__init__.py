2865d0bfae80936b86bd75931b8f482a08cd3e03
Florian Rau <bimac@users.noreply.github.com>
2023-09-14 18:30:00 +0100

    Update __init__.py

--- iblrig/__init__.py
+++ iblrig/__init__.py
@@ -1,3 +1,8 @@
+from pathlib import Path
 from setuptools_scm import get_version
+from importlib.metadata import version
 
-__version__ = get_version(version_scheme='post-release', local_scheme='dirty-tag', root='../..')
+if Path('.github').exists():
+    __version__ = get_version(version_scheme='post-release', local_scheme='dirty-tag')
+else:
+    __version__ = version('iblrig')
