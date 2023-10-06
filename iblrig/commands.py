a0b41302750f8ce97fffc035ae38a57a07ee3aa7
Florian Rau <bimac@users.noreply.github.com>
2023-10-06 15:53:35 +0100

    refactoring copiers

--- iblrig/commands.py
+++ iblrig/commands.py
@@ -2,14 +2,14 @@ import argparse
 import datetime
 import json
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Type
 
 import yaml
 import shutil
 
 from iblutil.util import setup_logger
 from ibllib.io import raw_data_loaders
-from iblrig.transfer_experiments import BehaviorCopier, VideoCopier, EphysCopier
+from iblrig.transfer_experiments import BehaviorCopier, VideoCopier, EphysCopier, SessionCopier
 import iblrig
 from iblrig.hardware import Bpod
 from iblrig.path_helper import load_settings_yaml, get_local_and_remote_paths
@@ -83,7 +83,7 @@ def transfer_data_cli():
     Command-line interface for transferring behavioral data to the local server.
     """
     args = _transfer_parser("Copy behavior data to the local server.").parse_args()
-    transfer_data(**vars(args))
+    transfer_data(**vars(args), interactive=True)
 
 
 def transfer_video_data_cli():
@@ -91,7 +91,7 @@ def transfer_video_data_cli():
     Command-line interface for transferring video data to the local server.
     """
     args = _transfer_parser("Copy video data to the local server.").parse_args()
-    transfer_video_data(**vars(args))
+    transfer_video_data(**vars(args), interactive=True)
 
 
 def transfer_ephys_data_cli():
@@ -99,61 +99,108 @@ def transfer_ephys_data_cli():
     Command-line interface for transferring ephys data to the local server.
     """
     args = _transfer_parser("Copy ephys data to the local server.").parse_args()
-    transfer_ephys_data(**vars(args))
+    transfer_ephys_data(**vars(args), interactive=True)
 
 
-def _get_subjects_folders(local_path: Path, remote_path: Path) -> Tuple[Path, Path]:
+def _get_subjects_folders(local_path: Path, remote_path: Path, interactive: bool = False) -> Tuple[Path, Path]:
     rig_paths = get_local_and_remote_paths(local_path, remote_path)
     local_path = rig_paths.local_subjects_folder
     remote_path = rig_paths.remote_subjects_folder
     assert isinstance(local_path, Path)
     if remote_path is None:
         raise Exception("Remote Path is not defined.")
-    print(f'Local Path:  `{local_path}`')
-    print(f'Remote Path: `{remote_path}`\n')
+    if interactive:
+        print(f'Local Path:  `{local_path}`')
+        print(f'Remote Path: `{remote_path}`\n')
+    else:
+        logger.debug(f'Local Path:  `{local_path}`')
+        logger.debug(f'Remote Path: `{remote_path}`')
     return local_path, remote_path
 
 
-def _get_session_paths(local_path: Path, glob_pattern: str = 'transfer_me.flag',
-                       print_summary: bool = True) -> List[Path]:
-    session_paths = [f.parent for f in local_path.rglob(glob_pattern)]
-    if len(session_paths) == 0:
+def _get_copiers(copier: Type[SessionCopier], local_folder: Path, remote_folder: Path, lab: str = None,
+                 glob_pattern: str = 'transfer_me.flag', interactive: bool = False) -> List[SessionCopier]:
+
+    # get local/remote subjects folder
+    rig_paths = get_local_and_remote_paths(local_path=local_folder, remote_path=remote_folder, lab=lab)
+    local_subjects_folder = rig_paths.local_subjects_folder
+    remote_subjects_folder = rig_paths.remote_subjects_folder
+    assert isinstance(local_subjects_folder, Path)
+    if remote_subjects_folder is None:
+        raise Exception("Remote Path is not defined.")
+    if interactive:
+        print(f'Local Path:  `{local_subjects_folder}`')
+        print(f'Remote Path: `{remote_subjects_folder}`\n')
+    else:
+        logger.debug(f'Local Path:  `{local_subjects_folder}`')
+        logger.debug(f'Remote Path: `{remote_subjects_folder}`')
+
+    # get copiers
+    copiers = [copier(f.parent, remote_subjects_folder) for f in local_subjects_folder.rglob(glob_pattern)]
+    if len(copiers) == 0:
         print('Could not find any sessions to copy to the local server.')
-        session_paths = list()
-    elif print_summary:
-        print('Going to copy following sessions to the local server:')
-        for session_path in session_paths:
-            print(f' * {session_path}')
-        if input('Do you want to continue? [Y/n]  ') not in ('y', ''):
-            session_paths = list()
-    return session_paths
+    elif interactive:
+        _print_status(copiers, 'Session states prior to transfer operation:')
+        if input('\nDo you want to continue? [Y/n]  ') not in ('y', ''):
+            copiers = list()
+    return copiers
 
 
-def transfer_ephys_data(local_path: Path = None, remote_path: Path = None, dry: bool = False):
-    local_subject_folder, remote_subject_folder = _get_subjects_folders(local_path, remote_path)
+def _print_status(copiers: Iterable[SessionCopier], heading: str = '') -> None:
+    print(heading)
+    for copier in copiers:
+        match copier.state:
+            case 0:
+                state = 'not registered on server'
+            case 1:
+                state = 'copy pending'
+            case 2:
+                state = 'copy complete'
+            case 3:
+                state = 'copy finalized'
+            case _:
+                state = 'undefined'
+        print(f' * {copier.session_path}: {state}')
 
-    for session_path in _get_session_paths(local_subject_folder):
-        vc = EphysCopier(session_path, remote_subjects_folder=remote_subject_folder)
-        logger.critical(f"{vc.state}, {vc.session_path}")
+
+def transfer_ephys_data(local_path: Path = None, remote_path: Path = None, dry: bool = False,
+                        interactive: bool = False):
+    local_subject_folder, remote_subject_folder = _get_subjects_folders(local_path, remote_path, interactive)
+    copiers = _get_copiers(EphysCopier, local_path, remote_path, interactive=interactive)
+
+    for copier in copiers:
+        logger.critical(f"{copier.state}, {copier.session_path}")
         if not dry:
-            vc.run()
+            copier.run()
+
+    if interactive:
+        _print_status(copiers, 'States after transfer operation:')
+
+    # once we copied the data, remove older session for which the data was successfully uploaded
     remove_local_sessions(weeks=2, local_path=local_subject_folder,
                           remote_path=remote_subject_folder, dry=dry, tag='ephys')
 
 
-def transfer_video_data(local_path: Path = None, remote_path: Path = None, dry: bool = False):
-    local_subject_folder, remote_subject_folder = _get_subjects_folders(local_path, remote_path)
+def transfer_video_data(local_path: Path = None, remote_path: Path = None, dry: bool = False,
+                        interactive: bool = False):
+    local_subject_folder, remote_subject_folder = _get_subjects_folders(local_path, remote_path, interactive)
+    copiers = _get_copiers(VideoCopier, local_path, remote_path, interactive=interactive)
 
-    for session_path in _get_session_paths(local_subject_folder):
-        vc = VideoCopier(session_path, remote_subjects_folder=remote_subject_folder)
-        logger.critical(f"{vc.state}, {vc.session_path}")
+    for copier in copiers:
+        logger.critical(f"{copier.state}, {copier.session_path}")
         if not dry:
-            vc.run()
+            copier.run()
+
+    if interactive:
+        _print_status(copiers, 'Session states after transfer operation:')
+
+    # once we copied the data, remove older session for which the data was successfully uploaded
     remove_local_sessions(weeks=2, local_path=local_subject_folder,
                           remote_path=remote_subject_folder, dry=dry, tag='video')
 
 
-def transfer_data(local_path: Path = None, remote_path: Path = None, dry: bool = False, lab: str = None) -> None:
+def transfer_data(local_path: Path = None, remote_path: Path = None, dry: bool = False,
+                  interactive: bool = False) -> None:
     """
     Copies the behavior data from the rig to the local server if the session has more than 42 trials
     If the hardware settings file contains MAIN_SYNC=True, the number of expected devices is set to 1
@@ -176,12 +223,14 @@ def transfer_data(local_path: Path = None, remote_path: Path = None, dry: bool =
     -------
     None
     """
-    local_subject_folder, remote_subject_folder = _get_subjects_folders(local_path, remote_path)
     hardware_settings = load_settings_yaml('hardware_settings.yaml')
     number_of_expected_devices = 1 if hardware_settings.get('MAIN_SYNC', True) else None
 
-    for session_path in _get_session_paths(local_subject_folder):
-        sc = BehaviorCopier(session_path, remote_subjects_folder=remote_subject_folder)
+    local_subject_folder, remote_subject_folder = _get_subjects_folders(local_path, remote_path, interactive)
+    copiers = _get_copiers(BehaviorCopier, local_path, remote_path, interactive=interactive)
+
+    for copier in copiers:
+        session_path = copier.session_path
         task_settings = raw_data_loaders.load_settings(session_path, task_collection='raw_task_data_00')
         if task_settings is None:
             logger.info(f'skipping: no task settings found for {session_path}')
@@ -191,8 +240,8 @@ def transfer_data(local_path: Path = None, remote_path: Path = None, dry: bool =
             jsonable = session_path.joinpath('raw_task_data_00', '_iblrig_taskData.raw.jsonable')
             if not jsonable.exists():
                 logger.info(f'skipping: no task data found for {session_path}')
-                if sc.remote_session_path.exists():
-                    shutil.rmtree(sc.remote_session_path)
+                if copier.remote_session_path.exists():
+                    shutil.rmtree(copier.remote_session_path)
                 continue
             trials, bpod_data = load_task_jsonable(jsonable)
             ntrials = trials.shape[0]
@@ -215,11 +264,15 @@ def transfer_data(local_path: Path = None, remote_path: Path = None, dry: bool =
         # we check the number of trials acomplished. If the field is not there, we copy the session as is
         if task_settings.get('NTRIALS', 43) < 42:
             logger.info(f'Skipping: not enough trials for {session_path}')
-            if sc.remote_session_path.exists():
-                shutil.rmtree(sc.remote_session_path)
+            if copier.remote_session_path.exists():
+                shutil.rmtree(copier.remote_session_path)
             continue
-        logger.critical(f"{sc.state}, {sc.session_path}")
-        sc.run(number_of_expected_devices=number_of_expected_devices)
+        logger.critical(f"{copier.state}, {copier.session_path}")
+        copier.run(number_of_expected_devices=number_of_expected_devices)
+
+    if interactive:
+        _print_status(copiers, 'States after transfer operation:')
+
     # once we copied the data, remove older session for which the data was successfully uploaded
     remove_local_sessions(weeks=2, dry=dry, local_path=local_subject_folder, remote_path=remote_subject_folder)
 
