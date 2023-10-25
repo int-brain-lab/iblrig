4e16659dab3c0a3fa321ebdcd3c7f63267854516
Florian Rau <bimac@users.noreply.github.com>
2023-10-25 15:01:30 +0100

    1337

--- iblrig/hardware.py
+++ iblrig/hardware.py
@@ -182,7 +182,7 @@ class Bpod(BpodIO):
             t0 = time.monotonic()
             while time.monotonic() - t0 < .1 and self._arcom.serial_object.in_waiting == 0:
                 pass
-            if self._arcom.serial_object.in_waiting:
+            else:
                 self._arcom.serial_object.read(1)
 
     def valve(self, valve_id: int, state: bool):
