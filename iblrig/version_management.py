e5259225630409a935f65a7f6cc56a41e7911daf
Florian Rau <bimac@users.noreply.github.com>
2023-11-01 13:35:52 +0000

    make sure to call pip in the correct directory

--- iblrig/version_management.py
+++ iblrig/version_management.py
@@ -328,6 +328,6 @@ def upgrade() -> int:
         check_call(["git", "reset", "--hard"], cwd=BASE_DIR)
 
     check_call(["git", "pull", "--tags"], cwd=BASE_DIR)
-    check_call(["pip", "install", "-U", "pip"])
-    check_call(["pip", "install", "-U", "-e", "."])
+    check_call([sys.executable, "-m", "pip", "install", "-U", "pip"], cwd=BASE_DIR)
+    check_call([sys.executable, "-m", "pip", "install", "-U", "-e", BASE_DIR], cwd=BASE_DIR)
     return 0
