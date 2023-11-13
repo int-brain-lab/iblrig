7a1ce75168dec89358b2fc9b3fe16c4469667ebd
Florian Rau <bimac@users.noreply.github.com>
2023-11-13 16:34:00 +0000

    Update raw_data_loaders.py

--- iblrig/raw_data_loaders.py
+++ iblrig/raw_data_loaders.py
@@ -1,43 +1,32 @@
 import json
 import logging
+from typing import Any
 
 import pandas as pd
 
 log = logging.getLogger('iblrig')
 
 
-# class definition with no init is used as a namespace
-class jsonable:
-    def read(file, offset=None):
-        data = []
-        with open(file) as f:
-            if offset is not None:
-                f.seek(offset, 0)
-            for line in f:
-                data.append(json.loads(line))
-        return data
-
-    def _write(file, data, mode):
-        with open(file, mode) as f:
-            for obj in data:
-                f.write(json.dumps(obj) + '\n')
-
-    def write(file, data):
-        jsonable._write(file, data, 'w+')
-
-    def append(file, data):
-        jsonable._write(file, data, 'a')
-
-
-def load_task_jsonable(jsonable_file, offset=0):
+def load_task_jsonable(jsonable_file: str, offset: int | None = None) -> tuple[pd.DataFrame, list[Any]]:
     """
-    Reads in a task data jsonable file and returns a trials dataframe and a bpod data list
-    :param jsonable_file: a full-path to the jsonable file
-    :return:
-        trials_table: a pandas Dataframe with the trial info in the same format as the Session trials table
-        bpod_data: a list [n_trials] for each of the
+    Reads in a task data jsonable file and returns a trials dataframe and a bpod data list.
+
+    Parameters:
+    - jsonable_file (str): full path to jsonable file.
+    - offset (int or None): The offset to start reading from (default: None).
+
+    Returns:
+    - tuple: A tuple containing:
+        - trials_table (pandas.DataFrame): A DataFrame with the trial info in the same format as the Session trials table.
+        - bpod_data (list): timing data for each trial
     """
-    trials_table = jsonable.read(jsonable_file, offset=offset)
+    trials_table = []
+    with open(jsonable_file) as f:
+        if offset is not None:
+            f.seek(offset, 0)
+        for line in f:
+            trials_table.append(json.loads(line))
+
     # pop-out the bpod data from the table
     bpod_data = []
     for td in trials_table:
