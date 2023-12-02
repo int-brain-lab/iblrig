1474574a8b586df594c7c883b6c5357c2ed3856a
k1o0 <k1o0@3tk.co>
2023-12-02 15:29:31 +0000

    port_settings test

--- settings/port_settings.py
+++ settings/port_settings.py
@@ -1,7 +1,11 @@
 import argparse
 from pathlib import Path
-import yaml
 import json
+import re
+import warnings
+
+import yaml
+import one.params
 
 UPDATE_FIELDS = {
     None: {'RIG_NAME': 'NAME'},
@@ -11,14 +15,18 @@ UPDATE_FIELDS = {
     'device_rotary_encoder': {'COM_ROTARY_ENCODER'},
     'device_screen': {'DISPLAY_IDX', 'SCREEN_FREQ_TARGET', 'SCREEN_FREQ_TEST_DATE',
                       'SCREEN_FREQ_TEST_STATUS', 'SCREEN_LUX_DATE', 'SCREEN_LUX_VALUE'},
-
+    'device_valve': {'WATER_CALIBRATION_OPEN_TIMES', 'WATER_CALIBRATION_RANGE',
+                     'WATER_CALIBRATION_WEIGHT_PERDROP'},
+    'device_sound': {'OUTPUT'}
 }
 
 
 def main(v7_path=None, v8_path=None):
 
-    v7_path = v7_path or Path(Path.home().drive, '/', 'iblrig_params', '.iblrig_params.json')
-    v8_path = v8_path or Path(Path.home().drive, '/', 'iblrigv8', 'settings', 'iblrig_settings_template.yaml')
+    v7_path = v7_path or Path(Path.home().drive, '/', 'iblrig_params')
+    v7_path = v7_path / '.iblrig_params.json'
+    v8_path = v8_path or Path(Path.home().drive, '/', 'iblrigv8', 'settings')
+    v8_path = v8_path / 'iblrig_settings_template.yaml'
     v8_path_hw = v8_path.with_name('hardware_settings_template.yaml')
 
     with open(v7_path, 'r') as fp:
@@ -28,42 +36,59 @@ def main(v7_path=None, v8_path=None):
     with open(v8_path, 'r') as fp:
         v8_settings = yaml.safe_load(fp)
 
+    # Hardware settings
     v8_hw_settings['RIG_NAME'] = v7_settings['NAME']
+    v8_hw_settings['MAIN_SYNC'] = 'behavior' in v7_settings['NAME']
     for device, fields in UPDATE_FIELDS.items():
         if device is None:
             for new_field, old_field in fields.items():
                 v8_hw_settings[new_field] = v7_settings[old_field]
+        elif device == 'device_sound':
+            soundcard = 'xonar' if 'behavior' in v7_settings['NAME'] else 'harp'
+            v8_hw_settings['device_sound']['OUTPUT'] = soundcard
         else:
             for field in fields:
                 v8_hw_settings[device][field] = v7_settings[field]
 
-    v8_hw_settings['device_sound']['OUTPUT'] = 'xonar' if 'behavior' in v7_settings['NAME'] else 'harp'
-    v8_hw_settings['device_valve']['WATER_CALIBRATION_OPEN_TIMES'] = v7_settings['WATER_CALIBRATION_OPEN_TIMES']
-    v8_hw_settings['device_valve']['WATER_CALIBRATION_RANGE'] = v7_settings['WATER_CALIBRATION_RANGE']
-    v8_hw_settings['device_valve']['WATER_CALIBRATION_WEIGHT_PERDROP'] = v7_settings['WATER_CALIBRATION_WEIGHT_PERDROP']
-
     with open(v8_path_hw.with_name('hardware_settings.yaml'), 'w') as fp:
         yaml.safe_dump(v8_hw_settings, fp)
-    # with open(v8_path.with_name('iblrig_settings.yaml'), 'w') as fp:
-    #     yaml.safe_dump(fp, v8_settings)
+
+    # IBL rig settings
+    v8_settings['iblrig_local_data_path'] = v7_settings['DATA_FOLDER_LOCAL']
+    v8_settings['iblrig_remote_data_path'] = v7_settings['DATA_FOLDER_REMOTE']
+    match = re.match(r'^_iblrig_(\w+)_.*$', v7_settings['NAME'])
+    if match:
+        lab, = match.groups()
+        print(f'Settings ALYX_LAB as "{lab}"')
+        v8_settings['ALYX_LAB'] = lab
+    else:
+        warnings.warn('Unknown lab name, please manually update ALYX_LAB field.')
+
+    # TODO Attempt to load ONE settings for these fields
+    v8_settings['ALYX_URL'] = one.params.get_default_client()
+    v8_settings['ALYX_USER'] = one.params.get(client=v8_settings['ALYX_URL'], silent=True)
+
+    with open(v8_path.with_name('iblrig_settings.yaml'), 'w') as fp:
+        yaml.safe_dump(fp, v8_settings)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Port iblrigv7 settings to iblrigv8")
-    parser.add_argument('mouse', help='Mouse name')
     parser.add_argument(
-        "-t",
-        "--training",
-        default=False,
+        "-v7",
+        "--v7-path",
+        default=None,
         required=False,
-        action="store_true",
-        help="Launch video workflow for biasedCW session on ephys rig.",
+        type=Path,
+        help=r"The path to iblrigv7 params folder (default is C:\iblrig_params).",
     )
     parser.add_argument(
-        "--ignore-checks",
-        default=False,
+        "-v8",
+        "--v8-path",
+        default=None,
         required=False,
-        action="store_true",
-        help="Ignore ibllib and iblscripts checks",
+        type=Path,
+        help=r"The path to iblrigv8 settings (default is C:\iblrigv8\settings).",
     )
     args = parser.parse_args()
+    main(**vars(args))
