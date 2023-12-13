bd2f23f05ca8d41c80fbfba3b5980243e9942e36
Florian Rau <bimac@users.noreply.github.com>
2023-12-13 14:28:45 +0000

    ruff!

--- iblrig/test/test_constants.py
+++ iblrig/test/test_constants.py
@@ -2,7 +2,7 @@ import unittest
 
 from iblrig import constants
 
-class TestConstants(unittest.TestCase):
 
+class TestConstants(unittest.TestCase):
     def test_bonsai_folder_exists(self):
         assert constants.BONSAI_EXE.parent.exists()
