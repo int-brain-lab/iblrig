90fc5b08f23317dba29b68954d452fe83f4bb826
Florian Rau <bimac@users.noreply.github.com>
2024-02-06 10:37:06 +0000

    extra task parameters: fix for list of string

--- iblrig/gui/wizard.py
+++ iblrig/gui/wizard.py
@@ -712,7 +712,7 @@ class RigWizard(QtWidgets.QMainWindow, Ui_wizard):
                 elif arg.nargs == '+':
                     widget = QtWidgets.QLineEdit()
                     if arg.default:
-                        widget.setText(str(arg.default)[1:-1])
+                        widget.setText(', '.join(arg.default))
                     widget.editingFinished.connect(
                         lambda p=param, w=widget: self._set_task_arg(p, [x.strip() for x in w.text().split(',')])
                     )
@@ -763,7 +763,7 @@ class RigWizard(QtWidgets.QMainWindow, Ui_wizard):
 
             # some customizations
             match widget.property('parameter_dest'):
-                case 'probability_left':
+                case 'probability_left' | 'probability_opto_stim':
                     widget.setMinimum(0.0)
                     widget.setMaximum(1.0)
                     widget.setSingleStep(0.1)
@@ -826,9 +826,6 @@ class RigWizard(QtWidgets.QMainWindow, Ui_wizard):
         if layout.rowCount() == 0:
             layout.addRow(self.tr('(none)'), None)
             layout.itemAt(0, 0).widget().setEnabled(False)
-
-        # call timer to resize window
-        QtCore.QTimer.singleShot(1, lambda: self.resize(self.minimumSizeHint()))
 
     def _set_task_arg(self, key, value):
         self.task_arguments[key] = value
