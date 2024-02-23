ef51df5d689b79f63e2610083677274c4533370d
olivier <olivier.winter@hotmail.fr>
2024-02-23 09:03:28 +0000

    add tag release instructions

--- iblrig/__init__.py
+++ iblrig/__init__.py
@@ -4,6 +4,8 @@
 # 3) Check CI and eventually wet lab test
 # 4) Pull request to iblrigv8
 # 5) git tag the release in accordance to the version number below (after merge!)
+# >>> git tag 8.15.6
+# >>> git push origin --tags
 __version__ = '8.15.6'
 
 
