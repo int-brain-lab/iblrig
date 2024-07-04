63e5e5dd9aa1beb8464b2a268ca0a731db146b83
pdesanex <bimac@users.noreply.github.com>
2024-07-04 10:02:06 +0100

    filter warnings for FigureCanvasAgg

--- iblrig_tests/test_online_plots.py
+++ iblrig_tests/test_online_plots.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import matplotlib
 import numpy as np
+import pytest
 
 import iblrig.online_plots as op
 from iblrig.raw_data_loaders import load_task_jsonable
@@ -18,12 +19,14 @@ class TestOnlinePlots(unittest.TestCase):
         with zipfile.ZipFile(zip_jsonable, 'r') as zip:
             cls.task_file = Path(zip.extract('online_plots.jsonable', path=zip_jsonable.parent))
 
+    @pytest.mark.filterwarnings("ignore:FigureCanvasAgg")
     def test_during_task(self):
         myop = op.OnlinePlots()  # the canonical way is to provide a settings file, but this should run without
         trials_table, bpod_data = load_task_jsonable(self.task_file)
         for i in np.arange(trials_table.shape[0]):
             myop.update_trial(trials_table.iloc[i], bpod_data[i])
 
+    @pytest.mark.filterwarnings("ignore:FigureCanvasAgg")
     def test_from_existing_file(self):
         op.OnlinePlots()
         op.load_task_jsonable(jsonable_file=self.task_file)
