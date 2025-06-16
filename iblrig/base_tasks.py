36b060bcd166b45ff423a177ef9d4a1e98b6e998
Florian Rau <bimac@users.noreply.github.com>
2025-06-16 16:56:34 +0100

    Update base_tasks.py

--- iblrig/base_tasks.py
+++ iblrig/base_tasks.py
@@ -1252,6 +1252,7 @@ class NetworkSession(BaseSession):
             remote_rigs = {k: v for k, v in all_remote_rigs.items() if k in remote_rigs}
         # Load and connect to remote services
         self.connect(remote_rigs)
+        self.session_info.update('REMOTE_RIGS', remote_rigs)
         self.exp_ref = {}
         try:
             super().__init__(**kwargs)
