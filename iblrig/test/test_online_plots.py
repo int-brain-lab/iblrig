d76c36c9ffef56282287fe962baf4156c1cce97e
Florian Rau <bimac@users.noreply.github.com>
2025-07-08 14:08:27 +0100

    clean up

--- iblrig/test/test_online_plots.py
+++ iblrig/test/test_online_plots.py
@@ -9,10 +9,6 @@ import iblrig.gui.online_plots as op
 zip_jsonable = Path(__file__).parent.joinpath('fixtures', 'online_plots_biased_iblrigv7.zip')
 
 
-# @pytest.mark.skipif(
-#     os.getenv('GITHUB_ACTIONS') == 'true' and os.name == 'posix' and os.uname().sysname == 'Linux',
-#     reason='Skipping test on GitHub Actions Ubuntu runners',
-# )
 class TestOnlinePlots:
     @pytest.fixture
     def task_file(self):
