115028250c1cb3aba0c283b7082d0481f22338a1
Florian Rau <bimac@users.noreply.github.com>
2026-01-28 21:13:28 +0000

    Ensure `transfer_me.flag` is created when `SKIP_EVENT_REPLAY` is True

--- iblrig_tasks/_iblrig_tasks_passiveChoiceWorld/task.py
+++ iblrig_tasks/_iblrig_tasks_passiveChoiceWorld/task.py
@@ -92,6 +92,9 @@ class Session(ChoiceWorldSession):
         # Run the passive part i.e. spontaneous activity and RFMapping stim
         self.run_passive_visual_stim(sa_time=timedelta(seconds=self.task_params['SPONTANEOUS_ACTIVITY_SECONDS']))
 
+        # Create the transfer flag.
+        self.paths.SESSION_FOLDER.joinpath('transfer_me.flag').touch()
+
         if self.task_params['SKIP_EVENT_REPLAY'] is True:
             log.info('Skipping replay of task events')
             return
@@ -125,8 +128,6 @@ class Session(ChoiceWorldSession):
                 self.bonsai_visual_udp_client.send_message(r'/re', byte_hide_stim)
             if self.stopped:
                 break
-        # Create the transfer flag when the task finishes.
-        self.paths.SESSION_FOLDER.joinpath('transfer_me.flag').touch()
 
 
 if __name__ == '__main__':  # pragma: no cover
