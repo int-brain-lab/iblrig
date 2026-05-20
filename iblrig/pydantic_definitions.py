c7bc45856738551981156cd0ccada54223bd0642
Florian Rau <bimac@users.noreply.github.com>
2026-05-20 10:43:47 +0100

    Allow DISPLAY_IDX to be zero by changing type from PositiveInt to NonNegativeInt

--- iblrig/pydantic_definitions.py
+++ iblrig/pydantic_definitions.py
@@ -12,6 +12,7 @@ from pydantic import (
     DirectoryPath,
     Field,
     FilePath,
+    NonNegativeInt,
     PlainSerializer,
     PositiveFloat,
     PositiveInt,
@@ -112,7 +113,7 @@ class HardwareSettingsRotaryEncoder(BunchModel):
 
 
 class HardwareSettingsScreen(BunchModel):
-    DISPLAY_IDX: PositiveInt
+    DISPLAY_IDX: NonNegativeInt
     SCREEN_RESOLUTION: tuple[PositiveInt, PositiveInt] = (2048, 1536)
     SCREEN_FREQ_TARGET: PositiveInt
     SCREEN_FREQ_TEST_DATE: date | None = None
